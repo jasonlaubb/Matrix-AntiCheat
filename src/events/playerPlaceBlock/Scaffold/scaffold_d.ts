5855f7173322cbbf03ff91bec094abe5bda821af
jasonlaubb <136591629+jasonlaubb@users.noreply.github.com>
2023-09-23 19:33:17 +0800

    Change scaffold/D

--- src/events/playerPlaceBlock/Scaffold/scaffold_d.ts
+++ src/events/playerPlaceBlock/Scaffold/scaffold_d.ts
@@ -1,31 +1,39 @@
-import { Block, BlockType, Player, world } from '@minecraft/server';
-import config from '../../../data/config.js';
-import { flag, getScore, addScore, clearScore, uniqueId, punish } from '../../../util/World.js';
+import { world, system, Player, Block } from "@minecraft/server";
+import config from "../../../data/config.js";
+import { addScore, clearScore, flag, getGamemode, getScore, punish, uniqueId } from "../../../util/World.js";
+
+const lastBlockPlace = new Map<string, number>();
 
 const scaffold_d = () => {
-    const EVENT = world.beforeEvents.playerPlaceBlock.subscribe(ev => {
-      const player: Player = ev.player;
-      if(uniqueId(player)) return;
-      const block: Block = ev.block;
-      const block2: Block = player.dimension.getBlock({ x: Math.trunc(player.location.x), y: Math.trunc(player.location.y) - 1, z: Math.trunc(player.location.z)});
-      const block3: Block = player.dimension.getBlock({ x: Math.trunc(player.location.x), y: Math.trunc(player.location.y) - 2, z: Math.trunc(player.location.z)});
-      const speedlimit: number = Math.sqrt((config.modules.scaffoldD.illegalSpeed ** 2) / 2);
-      addScore(player, 'anticheat:scaffoldDgotcheck', 1);
-      if(getScore(player, 'anticheat:scaffoldDgotcheck') <= config.modules.scaffoldD.gotcheck) return;
-      clearScore(player, 'anticheat:scaffoldDgotcheck');
-      if (block3.typeId == 'minecraft:air' && block == block2 && player.getVelocity().x > speedlimit && player.getVelocity().z > speedlimit && !player.hasTag('anticheat:damaged') && !player.isFlying) {
-        block.setType({id: 'minecraft:air'} as BlockType);
-        addScore(player, 'anticheat:scaffoldDVl', 1);
-        flag(player, 'scaffold/D', getScore(player, 'anticheat:scaffoldDVl'));
-        if(getScore(player, 'anticheat:scaffoldDVl') > config.modules.scaffoldD.VL) {
-          clearScore(player, 'anticheat:scaffoldDVl');
-          punish(player, 'scaffold/D', config.modules.scaffoldD.punishment)
-        }
-      }
-    });
-    if(!config.modules.scaffoldD.state) {
-      world.beforeEvents.playerPlaceBlock.unsubscribe(EVENT);
+  const EVENT1 = world.beforeEvents.playerPlaceBlock.subscribe(ev => {
+    const player: Player = ev.player;
+    if(uniqueId(player) || player.getEffect('speed') || player.getEffect('jump_boost') || getGamemode(player) == 1) return;
+    const blockUnder: Block = player.dimension.getBlock({ x: player.location.x, y: player.location.y - 1, z: player.location.z });
+    const block: Block = ev.block;
+    if(blockUnder !== block) return;
+    if(lastBlockPlace.get(player.id) < 0 || Date.now() - lastBlockPlace.get(player.id) > 1) {
+      clearScore(player, 'anticheat:scaffoldDblockPlaced');
+      lastBlockPlace.set(player.id, Date.now() - lastBlockPlace.get(player.id));
+      return
     };
-  };
-  
-  export { scaffold_d }
+    addScore(player, 'anticheat:scaffoldDblockPlaced', 1);
+    system.runTimeout(() => {
+      addScore(player, 'anticheat:scaffoldDblockPlaced', -1);
+    }, 20);
+    if(getScore(player, 'anticheat:scaffoldDblockPlaced') > config.modules.scaffoldD.maxBlockBreakPerSecond) {
+      addScore(player, 'anticheat:scaffoldDVl', 1);
+      flag(player, 'Scaffold/A', getScore(player, 'anticheat:scaffoldDVl'));
+      player.teleport(player.location);
+      if(getScore(player, 'anticheat:scaffoldDVl') > config.modules.scaffoldD.VL) {
+        clearScore(player, 'anticheat:scaffoldDVl');
+        punish(player, 'Scaffold/A', config.modules.scaffoldD.punishment);
+      }
+    }
+  });
+  if(!config.modules.scaffoldD.state) {
+    world.beforeEvents.playerPlaceBlock.unsubscribe(EVENT1);
+    lastBlockPlace.clear()
+  }
+};
+
+export { scaffold_d }
