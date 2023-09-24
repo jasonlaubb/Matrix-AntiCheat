5c443575b24265a0ecb23a3c42358d17952fd57f
jasonlaubb <136591629+jasonlaubb@users.noreply.github.com>
2023-09-24 14:51:43 +0800

    killaura bad bad

--- src/events/entityHitEntity/Killaura/killaura_b.ts
+++ src/events/entityHitEntity/Killaura/killaura_b.ts
@@ -9,6 +9,8 @@ export const killaura_b = () => {
     const hitEntity = ev.hitEntity;
     const pos1 = { x: player.location.x, y: player.location.y, z: player.location.z };
     const pos2 = { x: hitEntity.location.x, y: hitEntity.location.y, z: hitEntity.location.z };
+    const distance = Math.sqrt((pos1.x - pos2.x) ** 2 + (pos1.z - pos2.z) ** 2);
+    if (distance <= 2) return;
     let angle = Math.atan2((pos2.z - pos1.z), (pos2.x - pos1.x)) * 180 / Math.PI - player.getRotation().y - 90;
     if (angle <= -180) angle += 360;
     angle = Math.abs(angle);
