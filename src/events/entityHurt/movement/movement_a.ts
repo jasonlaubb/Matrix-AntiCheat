716d3fe85cbae16eeb119c654e0c372f5f248da4
jasonlaubb <136591629+jasonlaubb@users.noreply.github.com>
2023-10-01 08:02:09 +0800

    Try to fix false flag

--- src/events/entityHurt/movement/movement_a.ts
+++ src/events/entityHurt/movement/movement_a.ts
@@ -6,7 +6,17 @@ const movement_a = () => {
   const EVENT = world.afterEvents.entityHurt.subscribe(ev => {
     const player = ev.hurtEntity as Player;
     if (player.typeId !== 'minecraft:player' || ev.damageSource.cause !== 'fall' || ev.damage <= 0) return;
-    if (!player.isOnGround) {
+    let canntApply = false;
+    for(let i = -1; i < 1; i++) {
+      for(let i2 = -1; i2 < 1; i++) {
+        if(!player.dimension.getBlock({ x: player.location.x + i, y: player.location.y - 1, z: player.location.z + i }).isAir()) {
+          canntApply = true;
+          break;
+      }
+      if(canntApply) break
+    };
+    if (!player.isOnGround && !canntApply) {
+      
       addScore(player, 'anticheat:movementAVl', 1);
       flag(player, 'movement/A', getScore(player, 'anticheat:movementAVl'));
       if (getScore(player, 'anticheat:movementAVl') > config.modules.movementA.VL) {
