f468b7aa5fcbdc34c77150c07203c0b4f3e6c783
jasonlaubb <136591629+jasonlaubb@users.noreply.github.com>
2023-10-02 10:17:46 +0800

    Update nokararos.ts

--- src/nokararos.ts
+++ src/nokararos.ts
@@ -12,10 +12,7 @@ version: 1.0.0-beta-Test
 license: GPL-v3.0
 */
 
-export const data = new Map<string | boolean, number>();
-
 //import all used file
-
 import { registData } from './events/worldInitialize/register.js';
 import { watchDog } from './events/systemEvents/Doge/watchDog.js';
 import { join_event } from './events/playerSpawn/join_events.js';
