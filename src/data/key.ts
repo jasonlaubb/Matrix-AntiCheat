e423296b9a3a18412404a41e4c7bf21b4c4d8b24
jasonlaubb <136591629+jasonlaubb@users.noreply.github.com>
2023-10-06 23:45:12 +0800

    Delete src directory

--- src/data/key.ts
+++
@@ -1,9 +0,0 @@
-export default {
-  "environment": {
-    "type": 0 //0: local world, 1: Realm, 2: Server
-  },
-  "operator": {
-    "password": 'Password123', //Use 64cat to protect the admin tag
-    "deop": false //deop who don't have permisson
-  }
-}
