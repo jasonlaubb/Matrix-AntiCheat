6b003b0bb04ec0f4d0d2fde998a913412b446191
jasonlaubb <136591629+jasonlaubb@users.noreply.github.com>
2023-10-17 21:00:45 +0800

    Good Good code?

--- scripts/Commands/chatSend.js
+++ scripts/Commands/chatSend.js
@@ -10,33 +10,28 @@ world.beforeEvents.chatSend.subscribe(data => {
   const player = data.sender
   const msg = data.message
   const spam = world.scoreboard.getObjective("spam").getScore(player.scoreboardIdentity)
+
   if (msg.startsWith(prefix)) {
     Commands(player, msg)
     data.cancel = true
     return
   }
+
   if(player.hasTag("mute")) return system.run(() => player.sendMessage(`§e[§cMatrix§e]§c You're muted!`))
   if(spam > 0) return system.run(() => player.sendMessage(`§e[§cMatrix§e]§c Please slow down your messages!`))
-  if(spam == 0) {
+
+  if(spam === 0 && !player.hasTag("MatrixOP")) {
     system.run(() => {
-      if(msg.startsWith(prefix)) return
-      if(player.hasTag("MatrixOP")) return
       world.scoreboard.getObjective('spam').setScore(player.scoreboardIdentity, 50)
     })
   }
-  let rank;
-  try {
-    for(let tag of player.getTags()) {
-      if(tag.startsWith("rank:")) {
-        rank = tag.replaceAll("rank:", "")
-      }
-    }
-  } catch {}
+
+  const rank = player.getTags().filter(f => f.startsWith('rank:'))[0]
   if(rank == undefined) {
     rank = "§7Member"
   }
   data.cancel = true
   system.run(() => {
-    world.sendMessage(`§8[ §7${rank} §8] §7${player.name}§8: §r${msg.replaceAll("§k","")}`)
+    world.sendMessage(`§8[§7${rank}§8] §7${player.name}§8: §r${msg.replaceAll("§k","")}`)
   })
 })
