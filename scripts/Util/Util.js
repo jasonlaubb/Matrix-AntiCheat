1b76d4631a79ea086700ca968a2502d7ed87a498
jasonlaubb <136591629+jasonlaubb@users.noreply.github.com>
2023-11-07 21:33:47 +0800

    Update Util.js

--- scripts/Util/Util.js
+++ scripts/Util/Util.js
@@ -182,5 +182,9 @@ class Util {
   }
 }
 
+function _uuid() {
+  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {var r = Math.random()*16|0,v=c=='x'?r:r&0x3|0x8;return v.toString(16);});
+}
+
 //export the method class
-export { Detect, Util }
+export { Detect, Util, _uuid }
