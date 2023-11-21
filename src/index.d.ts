ff22d7c110ec7a8e795ec9c074def27df96f78c3
jasonlaubb <136591629+jasonlaubb@users.noreply.github.com>
2023-11-21 23:13:57 +0800

    Delete src/index.d.ts

--- src/index.d.ts
+++
@@ -1,8 +0,0 @@
-//still need @ts-expact-error
-declare module "@minecraft/server" {
-    interface Player {
-        blockData: any[];
-        threwTridentAt: number;
-        lastExplosionTime: number;
-    }
-}
