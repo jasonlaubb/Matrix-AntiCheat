72ff64c8fead2ce9e10bf8562aabb5ff6770083a
jasonlaubb <136591629+jasonlaubb@users.noreply.github.com>
2023-12-29 16:04:11 +0800

    Update Fly.ts

--- src/Modules/Movement/Fly.ts
+++ src/Modules/Movement/Fly.ts
@@ -42,7 +42,7 @@ async function AntiFly (player: Player, now: number) {
         if (velocity > 0.7) {
             ++velocityLog[player.id]
             lastVelocity.set(id, velocity)
-        } else if (velocity > 0)
+        } else if (velocity > 0 || (player.isOnGround && velocity == 0))
             velocityLog[player.id] = 0
         
         //if(velocity> 0.7) player.runCommand(`title @s actionbar xz = ${Math.hypot(x, z)}  | velocity  = ${velocity}  | ground = ${player. isOnGround} `)
