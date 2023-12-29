0d9c8fa4f48736523dc980696c09b1ec2ab2092b
jasonlaubb <136591629+jasonlaubb@users.noreply.github.com>
2023-12-29 09:10:51 +0800

    Teleport come

--- src/Modules/Movement/Fly.ts
+++ src/Modules/Movement/Fly.ts
@@ -49,10 +49,10 @@ async function AntiFly (player: Player, now: number) {
 
         const flyMovement = (velocityLog[player.id] > 0 && velocity <= 0) || (velocity < 0.7 && player.fallDistance < -1.5)
         
-        if (((velocityLog[player.id] > 0 && player.lastVelLog == velocityLog[player.id])) && flyMovement && !(player.lastExplosionTime && now - player.lastExplosionTime < 5500) && !(player.threwTridentAt && now - player.threwTridentAt < 5000) && !player.isFlying && !player.hasTag("matrix:slime") && !player.isGliding && !(jumpBoost && jumpBoost?.amplifier > 2) && !(levitation && levitation?.amplifier > 2) && velocity != 1 && !player.isOnGround && !getBlock(player,0,(-1),0).includes("stairs") && !getBlock(player,0,0,0).includes("stairs")) {
+        if (((velocityLog[player.id] > 0 && player.lastVelLog == velocityLog[player.id])) && flyMovement && !(player.lastExplosionTime && now - player.lastExplosionTime < 5500) && !(player.threwTridentAt && now - player.threwTridentAt < 5000) && !player.isFlying && !player.hasTag("matrix:slime") && !player.isGliding && !(jumpBoost && jumpBoost?.amplifier > 2) && !(levitation && levitation?.amplifier > 2) && velocity != 1 && !player.isOnGround && !getBlock(player,0,(-1),0).includes("stairs")) && !getBlock(player,0,0,0).includes("stairs")) {
             const lastflag = lastFlag.get(id)
-            if (lastflag && now - lastflag <= 1500 && now - lastflag >= 60){
-                player.teleport(prevLoc)
+            player.teleport(prevLoc)
+            if (lastflag && now - lastflag <= 1500 && now - lastflag >= 60)
                 flag(player, "Fly", "A", config.antiFly.maxVL, config.antiFly.punishment, [lang(">velocityY") + ":" + +lastVelocity.get(id).toFixed(2)]);
             }
             velocityLog[player.id] = 0
