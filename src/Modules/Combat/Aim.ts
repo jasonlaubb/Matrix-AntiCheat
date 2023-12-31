a354eff90b6be2acddf6db9ed459f3cccc7d1a70
jasonlaubb <136591629+jasonlaubb@users.noreply.github.com>
2023-12-31 18:47:04 +0800

    Remove a useless statement

--- src/Modules/Combat/Aim.ts
+++ src/Modules/Combat/Aim.ts
@@ -40,7 +40,7 @@ async function AntiAim (player: Player) {
         }
 
         //B - false positive: very low, efficiency: mid
-        if (rotationSpeed.x === rotationSpeed.y && rotationSpeed.x !== rotationSpeed.y || rotationSpeed.x > 1 && rotationSpeed.y < 0.6 || rotationSpeed.x < 0.6 && rotationSpeed.y > 1) {
+        if (rotationSpeed.x > 1 && rotationSpeed.y < 0.6 || rotationSpeed.x < 0.6 && rotationSpeed.y > 1) {
             const timerSet = (timer.get(`aim-b:${player.id}`) || 0);
             timer.set(`aim-b:${player.id}`, timerSet + 1);
             if (timerSet > 30 && !player.hasTag("matrix:riding") && Math.abs(rotation.x) != 90) {
