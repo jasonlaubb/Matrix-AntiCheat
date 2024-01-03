09859dc0ce547f846b8d2b551334857f9fb0b939
jasonlaubb <136591629+jasonlaubb@users.noreply.github.com>
2024-01-03 18:19:18 +0800

    Update NoClip.ts

--- src/Modules/Movement/NoClip.ts
+++ src/Modules/Movement/NoClip.ts
@@ -93,7 +93,7 @@ async function AntiNoClip (player: Player, now: number) {
     const lastflag = lastFlag.get(id) 
     if (player?.lastSafePos && safePos && player?.lastClip && player?.backClip && player?.befoClip && (movementClip < 0.25 && player?.lastClip > config.antiNoClip.clipMove && player?.backClip < 0.25 || player.lastClip == player.backClip && player.backClip > config.antiNoClip.clipMove && movementClip < 0.25 && player.befoClip < 0.25) && (yV == 0 || Math.abs(yV) < 1.75 && player.isJumping) && !player.isGliding && !player.isFlying && !(player.lastExplosionTime && now - player.lastExplosionTime < 1000) && !(player.threwTridentAt && now - player.threwTridentAt < 2500) && !(player.lastApplyDamage && now - player.lastApplyDamage < 250)) {
         if (!config.slient) player.teleport(player.lastSafePos);
-        if (lastflag && Date.now() - lastflag < 1250){
+        if (lastflag && Date.now() - lastflag < 3000){
             flag(player, "NoClip", "A", config.antiNoClip.maxVL, config.antiNoClip.punishment, [lang(">velocityXZ") + ":" + movementClip.toFixed(2)]);
         } 
         lastFlag.set(id, now) 
