499c64c082275bbdf9041eda81b6924c037dc4ec
RaMiGamer <140818153+RaMiGamerDev@users.noreply.github.com>
2024-01-03 21:51:14 +0100

    Add files via upload

---
+++ src/Modules/Misc/ARAS.js
@@ -0,0 +1,55 @@
+import { world, system } from "@minecraft/server";
+import { ActionFormData } from  "@minecraft/server-ui";
+import { flag, c, isAdmin } from "../../Assets/Util";
+import lang from "../../Data/Languages/lang";
+/**
+ * @author RaMiGanerDev
+ * @description anti Crashary (ARAS) bot attack realms with bot press button detector
+*/
+const timer = new Map()
+const clickSpeed = new Map()
+const config = c();
+world.afterEvents.playerSpawn.subscribe((event)=>{
+  const spawn = event.intialSpawn
+  const player = event.player
+  if(!player.hasTag("matrix:dead")){
+  timer.set(player.id,Date.now())
+  player.runCommand(`ability @s mute true`)
+  player.addTag("matrix:notVerified")
+  player.sendMessage(`§bMatrix §7> §cyou cant chat untill you §averify §cclose chat and wait ui to show you have §e${config.antiARAS.timer} §cminute to §averify`)
+  }
+})
+
+system.runInterval(()=>{
+  for(let player of world.getPlayers()){
+    if(player.hasTag("matrix:notVerified")){
+    if(Date.now() - timer.get(player.id) >= config.antiARAS.timer*1000*60 && player.hasTag("matrix:notVerified")){
+      flag(player, "Verify", "A", 0, "kick");
+    }
+      clickSpeed.set(player.id,Date.now())
+    const UI = new ActionFormData()
+    UI.title("§aVerification")
+    UI.body("You have to verify you are not bot by press verify")
+    UI.button("§a§lVerify")
+    UI.show(player).then((result)=>{
+      if(result.cancled){
+        if(!player.hasTag("matrix:notVerified")) return
+      player.runCommand(`kick "${player.name}" §r\n§c§l$You have been kicked§r\n§7Reason: §eYou have to verify\n§7By: §eMatrix`)
+      } if(result.selection == 0 && Date.now() - clickSpeed.get(player.id) > config.antiARAS.clickSpeedThershold*50){
+        if(!player.hasTag("matrix:notVerified")) return
+        player.removeTag("matrix:notVerified")
+        player.runCommand(`ability @s mute false`)
+        player.sendMessage(`§bMatrix §7> §aYou have been verified successfully`)
+      } if(Date.now() - clickSpeed.get(player.id) <= config.antiARAS.clickSpeedThershold*50 && result.selection  == 0){
+        
+        flag(player, "ARAS (Crashary)", "A", config.antiARAS.maxVL, config.antiARAS.punishment, [lang(">Delay") + ":" + (Date.now() - clickSpeed.get(player.id) ).toFixed(2)]);
+      }
+    })
+   }
+  }
+},60)
+world.afterEvents.playerLeave.subscribe((playerId)=>{
+  const id = playerId
+  timer.delete(id)
+  clickSpeed.delete(id)
+})
