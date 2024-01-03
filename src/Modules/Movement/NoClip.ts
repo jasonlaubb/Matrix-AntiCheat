e0aec99a096e59cc36292d133998b9be90f0c424
jasonlaubb <136591629+jasonlaubb@users.noreply.github.com>
2024-01-03 15:10:31 +0800

    Ended false positive of anti NoClip

--- src/Modules/Movement/NoClip.ts
+++ src/Modules/Movement/NoClip.ts
@@ -32,7 +32,7 @@ const powderBlock = [
 ] as string[]
 
 const safeLocation = new Map<string, Vector3>()
-const lastFlag = new Map<number>()
+const lastFlag = new Map<string, number>()
 const passableBlocks = [
     MinecraftBlockTypes.Sand,
     MinecraftBlockTypes.Gravel,
@@ -41,6 +41,33 @@ const passableBlocks = [
 
 const isSolidBlock = (block: Block) => {
     return block?.isSolid && !passableBlocks.includes(block?.typeId as MinecraftBlockTypes) && !powderBlock.includes(block?.typeId as MinecraftBlockTypes)
+}
+
+function blocksBetween (start: Vector3, end: Vector3, defaultY: number): Location[] {
+    const chunks: Vector3[] = [];
+    const { x: startX, z: startZ } = start;
+    const { x: endX, z: endZ } = end;
+
+    const dx = endX - startX;
+    const dz = endZ - startZ;
+
+    const stepsX = Math.abs(dx);
+    const stepsZ = Math.abs(dz);
+
+    const steps = Math.max(stepsX, stepsZ);
+
+    const xIncrement = stepsX === 0 ? 0 : dx / steps;
+    const zIncrement = stepsZ === 0 ? 0 : dz / steps;
+
+    for (let i = 1; i < steps; i++) {
+        chunks.push({
+            x: Math.floor(startX + xIncrement * i),
+            z: Math.floor(startZ + zIncrement * i),
+            y: defaultY,
+        });
+    }
+
+    return chunks;
 }
 
 /**
@@ -55,14 +82,17 @@ async function AntiNoClip (player: Player, now: number) {
     const { x: x2, y: y2, z: z2 } = player.location;
     const movementClip = Math.hypot(xV, zV);
     const lastPos = safeLocation.get(player.id);
-    const LastFlag = lastFlag.get(id) 
     if (player?.lastSafePos && lastPos && player?.lastClip && player?.backClip && player?.befoClip && (movementClip < 0.25 && player?.lastClip > config.antiNoClip.clipMove && player?.backClip < 0.25 || player.lastClip == player.backClip && player.backClip > config.antiNoClip.clipMove && movementClip < 0.25 && player.befoClip < 0.25) && (yV == 0 || Math.abs(yV) < 1.75 && player.isJumping) && !player.isGliding && !player.isFlying && !(player.lastExplosionTime && now - player.lastExplosionTime < 1000) && !(player.threwTridentAt && now - player.threwTridentAt < 2500) && !(player.lastApplyDamage && now - player.lastApplyDamage < 250)) {
-        if (!config.slient)
-            player.teleport(player.lastSafePos);
-        if(LastFlag && Date.now() - LastFlag){
-        flag(player, "NoClip", "A", config.antiNoClip.maxVL, config.antiNoClip.punishment, [lang(">velocityXZ") + ":" + movementClip.toFixed(2)]);
-            } 
-        lastFlag.set(id,Date.now()) 
+        // checks if player moved through at least a block
+        if (Math.abs(lastPos.y - y2) < 1.7 && x1 - z2 > 0.3 && blocksBetween(lastPos, player.location).some(loc => isSolidBlock(player.dimension.getBlock(loc)))) {
+            if (!config.slient) player.teleport(player.lastSafePos);
+
+            const Lastflag = lastFlag.get(id) 
+            if(Lastflag && Date.now() - Lastflag < 2500){
+               flag(player, "NoClip", "A", config.antiNoClip.maxVL, config.antiNoClip.punishment, [lang(">velocityXZ") + ":" + movementClip.toFixed(2)]);
+            }
+            lastFlag.set(id,Date.now())
+        }
     }
     /*if (player?.lastClip && player?.lastClip > 1.6) {
         player.sendMessage(`${player.befoClip} > ${player.backClip} > ${player.lastClip} > ${movementClip}`)
