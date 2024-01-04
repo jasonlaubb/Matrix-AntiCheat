667ccfb009cf442ddb77c6db8660dcaea8a572e2
jasonlaubb <136591629+jasonlaubb@users.noreply.github.com>
2024-01-04 23:57:42 +0800

    Update ARAS.js

--- src/Modules/Misc/ARAS.js
+++ src/Modules/Misc/ARAS.js
@@ -1,6 +1,6 @@
-import { world, system } from "@minecraft/server";
+import { world, system, PlayerSpawnAfterEvent } from "@minecraft/server";
 import { ActionFormData } from  "@minecraft/server-ui";
-import { flag, c, isAdmin } from "../../Assets/Util";
+import { flag, c, isAdmin, kick } from "../../Assets/Util";
 import lang from "../../Data/Languages/lang";
 
 /**
@@ -12,48 +12,56 @@ import lang from "../../Data/Languages/lang";
 
 const config = c();
 
-world.afterEvents.playerSpawn.subscribe((event) => {
-    const spawn = event.intialSpawn;
-    const player = event.player;
-    
-    if (spawn && !player.hasTag("matrix:dead")) {
-        player.verifyTimer ??= Date.now()
-        player.addTag("matrix:notVerified");
-        player.sendMessage(`§bMatrix §7> §cyou cant chat untill you §averify §cclose chat and wait ui to show you have §e${config.antiARAS.timer} §cminute to §averify`);
-    }
-});
+const playerSpawn = ({ intialSpawn: spawn, player }: PlayerSpawnAfterEvent) => {
+    // wait 0.1 seconds
+    system.runTimeout(() => {
+        if (spawn) {
+            player.verifyTimer ??= Date.now()
+            player.addTag("matrix:notVerified");
+            player.sendMessage(`§bMatrix §7> §cyou cant chat untill you §averify §cclose chat and wait ui to show you have §e${config.antiARAS.timer} §cminute to §averify`);
+        }
+    }, 2)
+};
 
-system.runInterval(() => {
-    for (let player of world.getPlayers()) {
-        if (player.hasTag("matrix:notVerified")) {
-            if (Date.now() - player.verifyTimer >= config.antiARAS.timer * 1000 * 60 && player.hasTag("matrix:notVerified")) {
-                flag(player, "Verify", "A", 0, "kick", undefined);
+const antiBot = () => {
+    const players = world.getPlayers({ tags: ["matrix:notVerified"] })
+    const now = Date.now()
+    for (const player of players) {
+            if (now - player.verifyTimer >= config.antiARAS.timer * 1000 * 60 && player.hasTag("matrix:notVerified")) {
+                kick (player, "Matrix AntiCheat", "failed to verify")
             }
             
             player.verifyClickSpeed = Date.now()
-            
-            new ActionFormData()
-            .title("Captcha")
-            .body("You need to verify that you're not a bot by clicking Verify")
-            .button("§a§l§¶Verify")   
-            .show(player).then((result) => {
-                if (result.cancled) {
-                    if (!player.hasTag("matrix:notVerified")) return;
-                    player.runCommand(`kick "${player.name}" §r\n§c§l$You have been kicked§r\n§7Reason: §eYou have to verify\n§7By: §eMatrix`);
-                } else if (result.selection == 0 && Date.now() - player.verifyClickSpeed > config.antiARAS.clickSpeedThershold * 50) {
-                    if (!player.hasTag("matrix:notVerified")) return;
-                    player.removeTag("matrix:notVerified");
-                    player.sendMessage(`§bMatrix §7> §aYou have been verified successfully`);
-                } else if (Date.now() - clickSpeed.get(player.id) <= config.antiARAS.clickSpeedThershold * 50 && result.selection == 0) {
-                    flag(player, "ARAS (Crashary)", "A", config.antiARAS.maxVL, config.antiARAS.punishment, [lang(">Delay") + ":" + (Date.now() - clickSpeed.get(player.id)).toFixed(2)]);
-                }
-            });
-        }
-    }
-}, 60);
 
-world.afterEvents.playerLeave.subscribe((playerId) => {
-    const id = playerId;
-    timer.delete(id);
-    clickSpeed.delete(id);
-});
+            try {
+                new ActionFormData()
+                .title("Anti Bot")
+                .body("You need to verify that you're not a bot by clicking Verify")
+                .button("§a§l§¶Verify")   
+                .show(player).then((result) => {
+                    if (result.cancled) {
+                        // call kick function after warning
+                    } else if (result.selection == 0 && Date.now() - player.verifyClickSpeed > config.antiARAS.clickSpeedThershold * 50) {
+                        if (!player.hasTag("matrix:notVerified")) return;
+                        player.removeTag("matrix:notVerified");
+                        player.sendMessage(`§bMatrix §7> §aYou have been verified successfully`);
+                    } else if (now - player.verifyClickSpeed <= config.antiARAS.clickSpeedThershold * 50 && result.selection == 0) {
+                        flag(player, "Crashary Bot", "A", config.antiARAS.maxVL, config.antiARAS.punishment, [lang(">Delay") + ":" + (Date.now() - clickSpeed.get(player.id)).toFixed(2)]);
+                    }
+                });
+            } catch { }
+    }
+}
+
+let id: number
+
+export default {
+    enable () {
+        id = system.runInterval(antiBot, 60)
+        world.afterEvents.playerSpawn.subscribe(playerSpawn)
+    },
+    disable () {
+        system.clearRun(id)
+        world.afterEvents.playerSpawn.unsubscribe(playerSpawn)
+    }
+}
