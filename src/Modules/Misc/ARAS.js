6e0510b257461c92ce43b46b509018e5bf25422c
jasonlaubb <136591629+jasonlaubb@users.noreply.github.com>
2024-01-04 21:05:08 +0800

    Update ARAS.js

--- src/Modules/Misc/ARAS.js
+++ src/Modules/Misc/ARAS.js
@@ -8,7 +8,6 @@ import lang from "../../Data/Languages/lang";
  * @description anti Crashary (ARAS) bot attack realms with bot press button detector
 */
 
-const timer = new Map();
 const clickSpeed = new Map();
 const config = c();
 
@@ -16,8 +15,8 @@ world.afterEvents.playerSpawn.subscribe((event) => {
     const spawn = event.intialSpawn;
     const player = event.player;
     
-    if (!player.hasTag("matrix:dead")) {
-        timer.set(player.id, Date.now());
+    if (spawn && !player.hasTag("matrix:dead")) {
+        player.verifyTimer ??= Date.now()
         player.addTag("matrix:notVerified");
         player.sendMessage(`§bMatrix §7> §cyou cant chat untill you §averify §cclose chat and wait ui to show you have §e${config.antiARAS.timer} §cminute to §averify`);
     }
@@ -26,18 +25,17 @@ world.afterEvents.playerSpawn.subscribe((event) => {
 system.runInterval(() => {
     for (let player of world.getPlayers()) {
         if (player.hasTag("matrix:notVerified")) {
-            if (Date.now() - timer.get(player.id) >= config.antiARAS.timer * 1000 * 60 && player.hasTag("matrix:notVerified")) {
-                flag(player, "Verify", "A", 0, "kick");
+            if (Date.now() - player.verifyTimer >= config.antiARAS.timer * 1000 * 60 && player.hasTag("matrix:notVerified")) {
+                flag(player, "Verify", "A", 0, "kick", undefined);
             }
             
             clickSpeed.set(player.id, Date.now());
             
-            const UI = new ActionFormData();
-            UI.title("Captcha");
-            UI.body("You need to verify that you're not a bot by clicking Verify");
-            UI.button("§a§l§¶Verify");
-            
-            UI.show(player).then((result) => {
+            new ActionFormData()
+            .title("Captcha")
+            .body("You need to verify that you're not a bot by clicking Verify")
+            .button("§a§l§¶Verify")   
+            .show(player).then((result) => {
                 if (result.cancled) {
                     if (!player.hasTag("matrix:notVerified")) return;
                     player.runCommand(`kick "${player.name}" §r\n§c§l$You have been kicked§r\n§7Reason: §eYou have to verify\n§7By: §eMatrix`);
