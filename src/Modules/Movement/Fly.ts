7c73d7d8512ca73a97c85653f2d91f19ed6e6ecc
RaMiGamer <140818153+RaMiGamerDev@users.noreply.github.com>
2024-01-16 17:33:35 +0100

    fixed fly/B false postives

--- src/Modules/Movement/Fly.ts
+++ src/Modules/Movement/Fly.ts
@@ -69,12 +69,7 @@ async function AntiFly(player: Player, now: number) {
 
 	if (jumpBoost?.amplifier > 2 || levitation?.amplifier > 2) return;
 	if (velocity > config.antiFly.maxVelocity) {
-		++velocityLog[player.id];
-		lastVelocity.set(id, velocity);
-	} else if (velocity > 0 || velocity == 0 && player.isOnGround) {
-		velocityLog[player.id] = 0
-	}
-
+	
 	// if (velocity> 0.7) player.runCommand(`title @s actionbar xz = ${Math.hypot(x, z)}  | velocity  = ${velocity}  | ground = ${player.isOnGround}`)
 
 	const flyMovement =
@@ -111,12 +106,17 @@ async function AntiFly(player: Player, now: number) {
 
 	player.lastVelLog = velocityLog[player.id];
 
-	if (player.lastVelocity && velocityLog[player.id] == 1 && velocity < 0 && player.lastVelocity > config.antiFly.maxVelocity && skip1 && skip2) {
+	if (player.lastVelocity && velocityLog[player.id] == 1 && velocity < 0 && player.lastVelocity > config.antiFly.maxVelocity && skip1 && skip2 && !instair) {
 		player.teleport(prevLoc);
 		flag(player, "Fly", "B", config.antiFly.maxVL, config.antiFly.punishment, [lang(">velocityY") + ":" + velocity.toFixed(4)]);
 	}
 
 	player.lastVelocity = velocity
+		++velocityLog[player.id];
+		lastVelocity.set(id, velocity);
+	} else if (velocity > 0 || velocity == 0 && player.isOnGround) {
+		velocityLog[player.id] = 0
+ }
 }
 
 const antiFly = () => {
