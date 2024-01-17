35e9480a0ee20c46279bc353a4bb922d74406ffe
RaMiGamer <140818153+RaMiGamerDev@users.noreply.github.com>
2024-01-17 16:27:32 +0100

    i will eat somth and i will continue coding

--- src/Functions/Ui Model/main.ts
+++ src/Functions/Ui Model/main.ts
@@ -9,4 +9,32 @@ const ui = new ActionFormData()
   ui.button("§cCommands") 
   ui.button("Config") 
   ui.button("About") 
+  ui.show(attacker).then(result => {
+    if(result.selection == 0){
+       commandUI(player) 
+  } if(result.selection == 1){
+       configUI(player) 
+  } if(result.selection == 2){
+       aboutUI(player) 
+  } 
+ }) 
+} 
+function commandUI(player){
+  const ui = new ActionFormData() 
+  ui.title("Command UI") 
+  ui.body("Admin tool help you use commands easily")
+  ui.button("ban") 
+  ui.button("unban") 
+  ui.button("lockdown") 
+  ui.button("invsee") 
+  ui.button("invcopy") 
+  ui.button("kick") 
+  ui.button("freeze") 
+  ui.button("unfreeze") 
+  ui.button("mute") 
+  ui.button("unmute") 
+} 
+function configUI(player){
+  ui.title("config UI") 
+  ui.body("Admin tool help you enable and disable easily")
   } 
