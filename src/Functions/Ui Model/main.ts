39ddc15a048ba5fab7cdd198e2b558647ce8b2af
RaMiGamer <140818153+RaMiGamerDev@users.noreply.github.com>
2024-01-17 15:31:45 +0100

    i will check to show ui to the player

--- src/Functions/Ui Model/main.ts
+++ src/Functions/Ui Model/main.ts
@@ -1,1 +1,12 @@
 // It's empty! Let fill 100k line!
+//lets do that
+import { world, system } from "@minecraft/server"
+import { ActionFormData } from "@minecraft/server-ui"
+function UI(player){
+const ui = new ActionFormData() 
+  ui.title("Admin gui") 
+  ui.body("Admin tool for manage matrix easily") 
+  ui.button("§cCommands") 
+  ui.button("Config") 
+  ui.button("About") 
+  } 
