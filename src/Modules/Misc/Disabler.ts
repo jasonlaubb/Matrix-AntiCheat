d7d6605ec8f5c35b6a46796ee77651168070bf6c
jasonlaubb <136591629+jasonlaubb@users.noreply.github.com>
2024-02-08 16:41:28 +0800

    AWA

--- src/Modules/Misc/Disabler.ts
+++ src/Modules/Misc/Disabler.ts
@@ -1,6 +1,6 @@
-import { EntityEquippableComponent, EquipmentSlot, ItemDurabilityComponent, PlayerSpawnAfterEvent, system, world } from "@minecraft/server"
+import { EntityDamageCause, EntityEquippableComponent, EntityHurtAfterEvent, EquipmentSlot, ItemDurabilityComponent, Player, PlayerSpawnAfterEvent, system, world } from "@minecraft/server"
 import { isAdmin, c, flag } from "../../Assets/Util"
-import { MinecraftItemTypes } from "../../node_modules/@minecraft/vanilla-data/lib/index"
+import { MinecraftEntityTypes, MinecraftItemTypes } from "../../node_modules/@minecraft/vanilla-data/lib/index"
 
 /**
  * @author jasonlaubb
@@ -13,10 +13,12 @@ export default {
     enable () {
         id = system.runInterval(antiDisabler)
         world.afterEvents.playerSpawn.subscribe(playerSpawn)
+        world.afterEvents.entityHurt.subscribe(entityHurt, { entityTypes: [MinecraftEntityTypes.Player] })
     },
     disable () {
         system.clearRun(id)
         world.afterEvents.playerSpawn.unsubscribe(playerSpawn)
+        world.afterEvents.entityHurt.unsubscribe(entityHurt)
     }
 }
 
@@ -37,6 +39,16 @@ const antiDisabler = () => {
         } else {
             player.lastNonGlidingPoint = player.location
         }
+    }
+}
+
+const entityHurt = ({ hurtEntity, damageSource: { damagingEntity, damagingProjectile, cause }}: EntityHurtAfterEvent) => {
+    if (isAdmin(hurtEntity as Player) || !damagingEntity || cause != EntityDamageCause.entityAttack || damagingProjectile || !damagingEntity) return;
+    if (hurtEntity.id == damagingEntity.id) {
+        const location = hurtEntity.location
+        system.run(() => hurtEntity.teleport(location))
+        const config = c()
+        flag (hurtEntity as Player, "Disabler", "B", config.antiDisabler.maxVL, config.antiDisabler.punishment, undefined)
     }
 }
 
