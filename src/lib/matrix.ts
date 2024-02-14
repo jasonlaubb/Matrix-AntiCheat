73a5cc5dc5bc128f5d8fb108338e38579f56a305
jasonlaubb <136591629+jasonlaubb@users.noreply.github.com>
2024-02-14 20:05:29 +0800

    Finish flag, isAdmin

--- src/lib/matrix.ts
+++ src/lib/matrix.ts
@@ -1,5 +1,7 @@
 import { FlagComponent } from "../data/interface";
 import { lang } from "./language";
+import { Player, world } from "@minecraft/server";
+import config from "../data/config";
 
 const flagData = new Map<string, { [key: string]: number }>()
 
@@ -13,6 +15,34 @@ export default class {
         playerFlag[id] ??= 0
         playerFlag[id] += 1
         const flagMessage = `§bMatrix §7>§c ${player.name}§g ` + lang(".Util.has_failed") + ` §4${id}§r §7[§c${lang(">Type")} ${type}§7] §7[§dx${playerFlag}§7]§r` + shown
-        // flagmode unfinish
+        const flagMode = world.getDynamicProperty("flagMode") ?? config.antiCheatOptions.flagMode
+        switch (flagMode) {
+            case "tag": {
+                const targets = world.getPlayers({ tags: ["matrix:notify"]})
+                targets.forEach(players => players.sendMessage(flagMessage))
+                break
+            }
+            case "bypass": {
+                const targets = world.getPlayers({ excludeNames: [player.name] })
+                targets.forEach(players => players.sendMessage(flagMessage))
+                break
+            }
+            case "admin": {
+                const allPlayers = world.getAllPlayers()
+                const targets = allPlayers.filter(players => this.isAdmin(players))
+                targets.forEach(players => players.sendMessage(flagMessage))
+                break
+            }
+            case "none": {
+                break
+            }
+            default: {
+                world.sendMessage(flagMessage)
+                break
+            }
+        }
+    }
+    static isAdmin (player: Player) {
+        return !!player.getDynamicProperty("isAdmin")
     }
 }
