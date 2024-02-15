b86c3b094724a2da0877d0aac0b8adb056c4bfbe
no1hereDev <147976037+no1hereDev@users.noreply.github.com>
2024-02-15 14:57:21 +0700

    Update about.ts

--- src/cc/commands/about.ts
+++ src/cc/commands/about.ts
@@ -3,9 +3,9 @@ import { lang } from "../../lib/language";
 import version from "../../version"
 const command = new Command(data => data
     .setName("about")
-    .description(lang("-help.about"))
-    .usage()
-    .requires(() => true))
+    .setDescription(lang("-help.about"))
+    .setUsage()
+    .setRequires(() => true))
     .execute(({ sender: player }) => {
         player.sendMessage(`§bMatrix §7>§g ${lang("-about.line1")}\n§g${lang("-about.version")}: §cV${version.join('.')}\n§g${lang("-about.author")}: §cjasonlaubb\n§gGitHub: §chttps://github.com/jasonlaubb/Matrix-AntiCheat`)
     })
