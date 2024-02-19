e27d32a62e638d6d687f316327f9019bb459a889
no1hereDev <147976037+no1hereDev@users.noreply.github.com>
2024-02-19 23:17:33 +0700

    add passwords command

---
+++ src/cc/commands/passwords.ts
@@ -0,0 +1,20 @@
+import { Command } from "../handler";
+import { lang } from "../../lib/language";
+//import { SHA256 } from ** SOS **;
+import { world, system } from "@minecraft/server";
+
+const command = new Command(data => data
+  .setName("passwords")
+  .setDescription(lang("-help.passwords"))
+  .setUsage("oldPassword", "newPassword")
+  .setAliases("setPassword", "pw"))
+  .option("string", option => option.setName("oldPassword"))
+  .option("string", option => option.setName("newPassword"))
+  .execute(({ sender: player }, [oldPassword, newPassword]) => {
+    const correctPassword = (world.getDynamicProperty("password") ?? config.commands.password);
+    if (oldPassword !== correctPassword) return system.run(() => player.sendMessage(`§bMatrix §7>§g ${lang("-passwords.wrong")}`));
+    world.sendMessage(`§bMatrix §7>§g ${player.name} ${lang("-passwords.changed")}`);
+    world.setDynamicProperty("sha_password", String(SHA256(newPassword)))
+  });
+
+Command.subscribe(command);
