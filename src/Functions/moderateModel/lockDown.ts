f4638088f824094ac35fead24ccc45d70e7aee0e
jasonlaubb <136591629+jasonlaubb@users.noreply.github.com>
2024-02-23 21:23:17 +0800

    Delete src directory

--- src/Functions/moderateModel/lockDown.ts
+++
@@ -1,8 +0,0 @@
-import { world } from "@minecraft/server";
-import { isAdmin, kick } from "../../Assets/Util";
-
-world.afterEvents.playerSpawn.subscribe(({ player, initialSpawn }) => {
-    if (world.getDynamicProperty("lockdown") !== true || !initialSpawn || isAdmin(player)) return;
-
-    kick (player, "Lock down enabled", 'Matrix AntiCheat')
-})
--- src/version.ts
+++
@@ -1,5 +0,0 @@
-export default [
-    4,
-    0,
-    0
-]
