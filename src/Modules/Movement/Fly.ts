b82fc986101922b367b9d1ab363a38b3354de392
jasonlaubb <136591629+jasonlaubb@users.noreply.github.com>
2024-03-29 14:23:03 +0800

    Rewrite of anti Fly, fixed false positive on it

--- src/Modules/Movement/Fly.ts
+++ src/Modules/Movement/Fly.ts
@@ -1,168 +1,132 @@
 import {
-	world,
-	system,
-	GameMode,
-	Player,
-	Vector3,
-	PlayerLeaveAfterEvent,
+    world,
+    system,
+    GameMode,
+    Vector3,
+    Dimension,
+    Player,
+    PlayerLeaveAfterEvent
 } from "@minecraft/server";
-import {
-	flag,
-	isAdmin,
-	c,
-	getPing
-} from "../../Assets/Util";
-import {
-	MinecraftEffectTypes
-} from "../../node_modules/@minecraft/vanilla-data/lib/index";
+import { flag, isAdmin, c, getPing } from "../../Assets/Util";
+import { MinecraftEffectTypes } from "../../node_modules/@minecraft/vanilla-data/lib/index";
 import lang from "../../Data/Languages/lang";
-import { tps } from "../../Assets/Public";
 
-const previousLocations = new Map <string, Vector3>();
-let velocityLog: { [key: string]: number } = {};
-const lastVelocity = new Map<string, number> ();
-const lastFlag = new Map <string, number>();
-const lastFlag2 = new Map<string, number>()
-
-/**
- * @author jasonlaubb && rami
- * @description This checks if a player velocity is too high.
- */
-const includeStair = ({ location: { x: px, y: py, z: pz }, dimension }: Player) => [dimension.getBlock({ x: Math.floor(px),
-		y: Math.floor(py),
-		z: Math.floor(pz),
-	})?.typeId,
-	dimension.getBlock({
-		x: Math.floor(px),
-		y: Math.floor(py) - 1,
-		z: Math.floor(pz),
-	})?.typeId
-].includes("stair");
-
-
-async function AntiFly(player: Player, now: number, Tps: number) {
-	const config = c();
-	//constant the infomation
-	const {
-		id,
-		isOnGround
-	} = player;
-
-	//get the previous location
-	const prevLoc = previousLocations.get(id);
-
-	//get the velocity
-	const {
-		y: velocity,
-		x,
-		z
-	} = player.getVelocity();
-
-	//if player is on ground and velocity is 0, set the previous location
-	if (isOnGround && velocity == 0 && x == 0 && z == 0) {
-		previousLocations.set(id, player.location);
-	}
-
-	const jumpBoost = player.getEffect(MinecraftEffectTypes.JumpBoost);
-	const levitation = player.getEffect(MinecraftEffectTypes.Levitation);
-	const instair = includeStair(player);
-
-	velocityLog[player.id] ??= 0;
-
-	if (prevLoc === undefined) return;
-
-	const skip1 = !(player.lastExplosionTime && now - player.lastExplosionTime < 5500) &&
-	!(player.threwTridentAt && now - player.threwTridentAt < 5000) && !player.hasTag("matrix:knockback");
-    const skip2 = !player.isFlying && !player.isGliding;
-    const skip3 = !(jumpBoost && jumpBoost?.amplifier > 2) &&
-	!(levitation && levitation?.amplifier > 2);
-
-	if (velocity > config.antiFly.maxVelocity && skip1) {
-	    velocityLog[player.id] += 1;
-	    lastVelocity.set(id, velocity);
-    } else if (velocity > 0 || velocity == 0 && player.isOnGround || !skip1)
-	    velocityLog[player.id] = 0
-
-	if (jumpBoost?.amplifier > 2 || levitation?.amplifier > 2) return;
-	//if (velocity> 0.7) player.runCommand(`title @s actionbar xz = ${Math.hypot(x, z)}  | velocity  = ${velocity}  | ground = ${player.isOnGround} | log = ${velocityLog[player.id]}`)
-
-	const flyMovement =
-		(velocityLog[player.id] > 1 && velocity <= 0) ||
-		(velocity < config.antiFly.maxVelocity && player.fallDistance < -1.5)
-	const clientFly =
-		velocityLog[player.id] > 0 && player?.lastVelLog == velocityLog[player.id];
-
-	if (
-		!player.isOnGround &&
-		clientFly &&
-		flyMovement &&
-		skip1 &&
-		skip2 &&
-		skip3 &&
-		velocity != 1 &&
-		!instair
-	) {
-		const lastflag = lastFlag.get(id);
-		player.teleport(prevLoc);
-
-		if (lastflag && now - lastflag <= 5000 && now - lastflag >= 500)
-			flag(player, "Fly", "A", config.antiFly.maxVL, config.antiFly.punishment, [lang(">velocityY") + ":" + lastVelocity.get(id).toFixed(2)]);
-		velocityLog[player.id] = 0;
-		lastVelocity.set(id, undefined);
-		lastFlag.set(id, now);
-	}
-
-	player.lastVelLog = velocityLog[player.id];
-
-	if (getPing(player) < 4 && Tps > 12 && player.lastVelocity && velocityLog[player.id] == 1 && velocity < 0 && player.lastVelocity > config.antiFly.maxVelocity && skip1 && skip2 && !instair) {
-		player.teleport(prevLoc);
-		const lastflag = lastFlag2.get(id)
-		if (lastflag && now - lastflag <= 4500 && now - lastflag > 120) {
-		    flag(player, "Fly", "B", config.antiFly.maxVL, config.antiFly.punishment, [lang(">velocityY") + ":" + velocity.toFixed(4)]);
-		}
-		lastFlag2.set(id, now)
-	}
-
-    player.lastVelocity = velocity
+interface FlyData {
+    previousLocations: Vector3
+    velocityLog: number
+    lastHighVelocity: number
+    previousHighVelocity: number
+    previousVelocity: number
+    lastFlag: number
+    lastFlag2: number
+    lastVelLog: number
+    lastVelocity: number
+}
+interface IncludeStairDataInput {
+    location: Vector3
+    dimension: Dimension
 }
 
-const antiFly = () => {
-	const now = Date.now();
-	const players = world.getPlayers({
-		excludeGameModes: [GameMode.spectator]
-	});
-	const Tps = tps.getTps()
-	for (const player of players) {
-		if (isAdmin(player)) continue;
+/**
+ * @author RaMiGamerDev
+ * @description Code rewrite by jasonlaubb
+ * A efficient vertical movement check based on bds prediction
+ */
 
-		AntiFly(player, now, Tps);
-	}
-};
-
-const playerLeave = ({
-	playerId
-}: PlayerLeaveAfterEvent) => {
-	previousLocations.delete(playerId);
-	lastVelocity.delete(playerId);
-	lastFlag.delete(playerId);
-	lastFlag2.delete(playerId);
-	delete velocityLog[playerId];
-};
-
-let id: number;
-
+const flyData = new Map<string, FlyData>()
+const includeStair = ({ location: { x: px, y: py, z: pz }, dimension }: IncludeStairDataInput) => {try{return[dimension.getBlock({ x: Math.floor(px),y: Math.floor(py),z: Math.floor(pz),})?.typeId,dimension.getBlock({x: Math.floor(px),y: Math.floor(py) - 1,z: Math.floor(pz),})?.typeId].includes("stair")}catch{return false}};
+function antiFly (player: Player, now: number) {
+    const config = c()
+    let data = flyData.get(player.id)
+    if (!data) {
+        data = {} as any
+        data = {
+            previousLocations: player.location,
+            velocityLog: 0,
+            lastVelLog: 0,
+            lastHighVelocity: 0,
+            previousHighVelocity: 0,
+            previousVelocity: undefined,
+            lastFlag: now,
+            lastFlag2: now
+        } as FlyData
+        flyData.set(player.id, data)
+        return
+    }
+    const { y: velocity, x, z } = player.getVelocity()
+    if (player.isOnGround && velocity == 0 && x == 0 && z == 0) {
+        data.previousLocations = player.location
+        data.velocityLog = 0
+    }
+    const jumpBoost = player.getEffect(MinecraftEffectTypes.JumpBoost);
+    const levitation = player.getEffect(MinecraftEffectTypes.Levitation);
+    const instair = includeStair(player);
+    const skip1 = !(player.lastExplosionTime && now - player.lastExplosionTime < 5500) &&
+    !(player.threwTridentAt && now - player.threwTridentAt < 5000) && !player.hasTag("matrix:knockback");
+    const skip2 = !player.isFlying && !player.isGliding;
+    const skip3 = !(jumpBoost && jumpBoost?.amplifier > 2) &&
+    !(levitation && levitation?.amplifier > 2);
+    if (jumpBoost?.amplifier > 2 || levitation?.amplifier > 2) {
+        flyData.set(player.id, data)
+        return
+    }
+    const flyMovement = (data.velocityLog > 1 && velocity <= 0) ||
+        (velocity < config.antiFly.maxVelocity && player.fallDistance < -1.5);
+    const clientFly = data.velocityLog > 0 && player?.lastVelLog == data.velocityLog;
+    if (!player.isOnGround &&
+        clientFly &&
+        flyMovement &&
+        skip1 &&
+        skip2 &&
+        skip3 &&
+        velocity != 1 &&
+        !instair) {
+        const lastflag = data.lastFlag;
+        player.teleport(data.previousLocations);
+        if (lastflag && now - lastflag <= 5000 && now - lastflag >= 500)
+            flag(player, "Fly", "A", config.antiFly.maxVL, config.antiFly.punishment, [lang(">velocityY") + ":" + data.lastHighVelocity.toFixed(2)]);
+        data.velocityLog = 0;
+        delete data.previousVelocity;
+        data.lastFlag = now
+    }
+    data.lastVelLog = data.velocityLog
+    if (getPing(player) < 4 && data.velocityLog == 1 && !instair && velocity <= 0 && player.lastVelocity <= 0) {
+        player.teleport(data.previousLocations);
+        data.previousVelocity = data.lastVelocity 
+        const lastflag = data.lastFlag2
+        if (lastflag && now - lastflag <= 4500 && now - lastflag > 120 && Math.abs(data.previousHighVelocity - data.lastHighVelocity) < 0.3 && Math.abs(player.lastVelocity - data.previousVelocity) > 0 ) {
+            flag(player, "Fly", "B", config.antiFly.maxVL, config.antiFly.punishment, [lang(">velocityY") + ":" + velocity.toFixed(4)]);
+        }
+        data.lastFlag2 = now
+    }
+    if (velocity > config.antiFly.maxVelocity && skip1) {
+        data.velocityLog += 1;
+        data.previousHighVelocity = data.lastHighVelocity
+        data.lastHighVelocity = velocity 
+    } else if (velocity >= 0 || velocity == 0 && player.isOnGround || !skip1) data.velocityLog = 0;
+    if (!(velocity > config.antiFly.maxVelocity || data.velocityLog == 1 && velocity <= 0)) player.lastVelocity = velocity;
+    flyData.set(player.id, data)
+}
+function systemEvent () {
+    const now = Date.now()
+    const players = world.getPlayers({ excludeGameModes: [GameMode.creative, GameMode.spectator] })
+    for (const player of players) {
+        if (isAdmin(player)) continue
+        antiFly (player, now)
+    }
+}
+function playerLeaveAfterEvent ({ playerId }: PlayerLeaveAfterEvent) {
+    flyData.delete(playerId)
+}
+let id: number
 export default {
-	enable() {
-		id = system.runInterval(antiFly, 1);
-		world.afterEvents.playerLeave.subscribe(playerLeave);
-	},
-	disable() {
-		previousLocations.clear();
-		lastVelocity.clear();
-		lastFlag.clear();
-		lastFlag2.clear()
-		velocityLog = {};
-		system.clearRun(id);
-		world.afterEvents.playerLeave.unsubscribe(playerLeave);
-	},
-};
+    enable () {
+        id = system.runInterval(systemEvent)
+        world.afterEvents.playerLeave.subscribe(playerLeaveAfterEvent)
+    },
+    disable () {
+        flyData.clear()
+        system.clearRun(id)
+        world.afterEvents.playerLeave.unsubscribe(playerLeaveAfterEvent)
+    }
+}
