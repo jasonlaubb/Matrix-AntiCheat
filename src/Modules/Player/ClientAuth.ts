fc7a648a4ad7ccd07130631417ff472c2c2b291c
jasonlaubb <136591629+jasonlaubb@users.noreply.github.com>
2024-04-01 12:38:58 +0800

    **A**

---
+++ src/Modules/Player/ClientAuth.ts
@@ -0,0 +1,29 @@
+import { GameMode, PlayerSpawnAfterEvent, system, world } from "@minecraft/server";
+import { isAdmin, kick } from "../../Assets/Util";
+import lang from "../../Data/Languages/lang";
+
+/**
+ * @author jasonlaubb
+ * @description A simple client authentication to detect bot client
+ */
+function clientAuth ({ player, initialSpawn }: PlayerSpawnAfterEvent) {
+    if (!initialSpawn || isAdmin(player) || player.getGameMode() == GameMode.spectator) return
+    const currentLocation = JSON.stringify(player.location)
+    // Apply a small knockback to the player
+    player.applyKnockback(0, -1.5, 0, 1)
+    system.run(() => {
+        const locationNow = JSON.stringify(player.location)
+        if (locationNow == currentLocation) {
+            kick (player, "Client authentication failed", lang(".Bot.by"))
+            console.warn(`ClientAuth :: ${player.name} >> Bot client is blocked`)
+        }
+    })
+}
+export default {
+    enable () {
+        world.afterEvents.playerSpawn.subscribe(clientAuth)
+    },
+    disable () {
+        world.afterEvents.playerSpawn.unsubscribe(clientAuth)
+    }
+}
