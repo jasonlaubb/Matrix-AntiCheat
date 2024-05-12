80f13e281bb6f2c5c30f2988ef6c077ee6adbd6f
jasonlaubb <jasonlau5378@gmai.com>
2024-05-12 17:52:04 +0800

    aaaaa

--- src/Functions/chatModel/Commands/import.ts
+++ src/Functions/chatModel/Commands/import.ts
@@ -1,5 +1,8 @@
 import "./helps/about";
 import "./helps/help";
 import "./moderations/op";
+import "./moderations/deop";
 import "./settings/toggles";
 import "./settings/toggle";
+import "./settings/passwords";
+import "./settings/flagmode";
---
+++ src/Functions/chatModel/Commands/settings/flagmode.ts
@@ -0,0 +1,22 @@
+import { world } from "@minecraft/server";
+import { registerCommand, sendRawText, verifier } from "../../CommandHandler";
+import { c } from "../../../../Assets/Util";
+
+registerCommand ({
+    name: "flagmode",
+    description: "Change the flag mode used for flag message in chat",
+    parent: false,
+    maxArgs: 1,
+    minArgs: 1,
+    argRequire: [
+        (value) => ["all", "tag", "bypass", "admin", "none"].includes(value as string)
+    ],
+    require: (player) => verifier (player, c().commands.flagmode),
+    executor: async (player, args) => {
+        world.setDynamicProperty("flagmode", args[0]);
+        sendRawText(player, 
+            { text: "§bMatrix §7>§g " },
+            { translate: "flagmode.changed", with: [args[0]] },
+        )
+    },
+})
