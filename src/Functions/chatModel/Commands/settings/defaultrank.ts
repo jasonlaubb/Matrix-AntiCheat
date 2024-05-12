86e110382438ecc5973ab60937e037951b4b6544
jasonlaubb <jasonlau5378@gmai.com>
2024-05-12 18:21:41 +0800

    aaaa a

---
+++ src/Functions/chatModel/Commands/settings/defaultrank.ts
@@ -0,0 +1,19 @@
+import { registerCommand, sendRawText, verifier } from "../../CommandHandler";
+import { c } from "../../../../Assets/Util";
+import { world } from "@minecraft/server";
+
+registerCommand ({
+    name: "defaultrank",
+    description: "Set the default rank",
+    parent: false,
+    maxArgs: 1,
+    minArgs: 1,
+    require: (player) => verifier (player, c().commands.defaultrank),
+    executor: async (player, args) => {
+        world.setDynamicProperty("defaultrank", args[0]);
+        sendRawText(player, 
+            { text: "§bMatrix §7>§g " },
+            { translate: "defaultrank.has", with: [args[0]] },
+        );
+    },
+})
