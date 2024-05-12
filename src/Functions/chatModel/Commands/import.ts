c2decaa1812ef5c526547cca757be0528cd4c0ba
jasonlaubb <jasonlau5378@gmai.com>
2024-05-12 18:09:19 +0800

    aaaaaaaaaaaaaaaaaaaaaaaaaaa

--- src/Functions/chatModel/Commands/import.ts
+++ src/Functions/chatModel/Commands/import.ts
@@ -2,6 +2,7 @@ import "./helps/about";
 import "./helps/help";
 import "./moderations/op";
 import "./moderations/deop";
+import "./moderations/rank"
 import "./settings/toggles";
 import "./settings/toggle";
 import "./settings/passwords";
