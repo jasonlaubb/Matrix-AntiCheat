843130949708c0f4121e4fa97b66de5d53b47ba9
jasonlaubb <136591629+jasonlaubb@users.noreply.github.com>
2024-05-25 21:30:59 +0800

    Barbarbar

--- src/Data/MessageCheckData.ts
+++ src/Data/MessageCheckData.ts
@@ -1,11 +1,11 @@
 export default {
     regExp: {
         link: {
-            contents: /^((http:\/\/|https:\/\/|www\.|download\.)([a-z]+\.)+[a-z]{2,8}(\/[\S]+)*\/*)|discord(\.gg|\.com\/invite)\/[\S]{1,}/igm,
+            contents: /((http:\/\/|https:\/\/|www\.|download\.)([a-z]+\.)+[a-z]{2,8}(\/[\S]+)*\/*)|discord(\.gg|\.com\/invite)\/[\S]{1,}/igm,
             endline: / +/g,
         },
         email: {
-            contents: /^.$/igm,
+            contents: /.$/igm,
             endline: /[\s|^(a-z|@|\.|0-9)]+/ig,
         },
     },
