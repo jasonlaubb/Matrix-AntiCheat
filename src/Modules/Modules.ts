a53864ba524bc7218e20d1bf3d5a3725db5617e3
jasonlaubb <jasonlau5378@gmai.com>
2024-05-26 20:42:56 +0800

    werfuregipnlwegrejhgnwiegewfjvwrejuic87aw4c49uwcphw9fw9hgw9jvf 0bwrj98ghw4igweu9hfqw9epscuk32,2c81g275984y682

--- src/Modules/Modules.ts
+++ src/Modules/Modules.ts
@@ -1,5 +1,5 @@
-import { c } from "../Assets/Util";
-// import the toggle handler of each module
+// old system
+import { c, isAdmin } from "../Assets/Util";
 import autoClicker from "./Combat/Auto Clicker";
 import killAura from "./Combat/Kill Aura";
 import reach from "./Combat/Reach";
@@ -32,6 +32,8 @@ import xray from "./Misc/Xray";
 import worldBorder from "./Movement/World Border";
 import disabler from "./Misc/Disabler";
 import clientAuth from "./Player/ClientAuth";
+import { EntityQueryOptions, Player, system, world } from "@minecraft/server";
+import Default from "../Data/Default";
 
 interface toggleHandler {
     enable: () => void;
@@ -88,4 +90,122 @@ export function moduleStart() {
             handler.enable();
         }
     }
+}
+
+// new system
+
+/**
+ * @author jasonlaubb
+ * @description Module Handler
+ */
+
+let MODULES: Module[] = [];
+
+export function registerModule (id: string, checkAdmin: boolean, varargs: (Map<string, any> | { [key: string]: any })[], ...event: (TickEvent | WorldEvent)[]): void {
+    const tickEvent = (event as TickEvent[]).filter(ev => ev?.tickInterval)
+    const worldEvent = (event as WorldEvent[]).filter(ev => ev?.worldSignal)
+    MODULES.push({
+        id: id,
+        checkAdmin: checkAdmin,
+        tickEvent: tickEvent.length > 0 ? tickEvent : undefined,
+        worldEvent: worldEvent.length > 0 ? worldEvent : undefined,
+        enabled: false,
+        mapclears: varargs.filter(arg => arg?.delete) as Map<string, any>[],
+    })
+}
+export function intilizeModules (): void {
+    const config = c();
+    let mapvalues: Map<string, any>[] = [];
+    MODULES.filter(module => module.enabled).forEach(module => {
+        unlisten(module.id);
+    })
+    MODULES.forEach(element => {
+        mapvalues.push(...element.mapclears);
+        if ((config as any)[element.id]?.enabled) {  // Method for state module is enabled
+            setup(config, element);
+        }
+    });
+}
+function setup (config: typeof Default, element: Module) {
+    let runIds = [];
+    if ((config as any)[element.id]?.enabled) {  // Method for state module is enabled
+        for (const tE of element.tickEvent) {
+            runIds.push(
+                system.runInterval(() => {
+                    const currentConfig = c();
+                    if (tE?.intick) {
+                        let allPlayers = tE?.playerOption ? world.getPlayers(tE.playerOption) : world.getAllPlayers();
+                        if (!element.checkAdmin) allPlayers = allPlayers.filter(player => !isAdmin(player));
+                        for (const player of allPlayers) {
+                            tE.intick(currentConfig, player).catch((error) => {
+                                rejected ("Module rejected :: " + element.id + " :: " + String(error));
+                            })
+                        }
+                    }
+                    if (tE?.onTick) {
+                        tE.onTick(currentConfig).catch((error) => {
+                            rejected ("Module rejected :: " + element.id + " :: " + String(error));
+                        })
+                    }
+                }, tE.tickInterval)
+            )
+        }
+        for (const wE of element.worldEvent) {
+            wE.worldSignal.subscribe((event: any) => {
+                const currentConfig = c();
+                wE.then(currentConfig, event).catch((error) => {
+                    rejected ("Module rejected :: " + element.id + " :: " + String(error));
+                })
+            })
+        }
+    }
+    element.runId = runIds;
+    element.enabled = true;
+    MODULES[MODULES.findIndex(a => a.id == element.id)] = element;
+}
+
+function unlisten (id: string) {
+    const index = MODULES.findIndex(a => a.id == id)
+    const module = MODULES[index];
+    if (!module) throw "Unlisten :: " + id + " :: No result";
+    if (!module?.enabled) "Unlisten :: " + id + " :: Already disabled";
+    for (const num of module.runId) {
+        system.clearRun(num);
+    }
+    for (const wor of module.worldEvent) {
+        wor.worldSignal.unsubscribe(wor.then);
+    }
+    for (const clear of module?.mapclears ?? []) {
+        clear.clear();
+    }
+    module.runId = [];
+    module.enabled = false;
+    MODULES[index] = module;
+}
+
+function rejected (error: string) {
+    const actualErrror = String(error);
+    console.warn(actualErrror);
+}
+
+interface Module {
+    id: string;
+    checkAdmin: boolean;
+    tickEvent?: TickEvent[];
+    worldEvent?: WorldEvent[];
+    runId?: number[]; // for module handler
+    enabled: boolean;
+    mapclears: Map<string, any>[];
+}
+
+interface TickEvent {
+    tickInterval: number;
+    intick?: (config: typeof Default, player: Player) => Promise<void | number>;
+    onTick?: (_config: typeof Default) => Promise<void | number>;
+    playerOption?: EntityQueryOptions;
+}
+interface WorldEvent {
+    worldSignal: any;
+    playerOption?: EntityQueryOptions;
+    then: (config: typeof Default, event: unknown) => Promise<void | number>;
 }
