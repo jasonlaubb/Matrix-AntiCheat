574330947179c6adfda69a25cc0fadcbdb5d3b6e
no1hereDev <147976037+no1hereDev@users.noreply.github.com>
2024-06-21 13:31:47 +0700

    Xray done

--- src/Modules/Misc/Xray.ts
+++ src/Modules/Misc/Xray.ts
@@ -1,6 +1,5 @@
 import { PlayerBreakBlockAfterEvent, world } from "@minecraft/server";
 import { c, flag, isAdmin, isTargetGamemode } from "../../Assets/Util";
-import lang from "../../Data/Languages/lang";
 
 const antiXray = ({
     player,
@@ -12,7 +11,7 @@ const antiXray = ({
     if (isAdmin(player) || isTargetGamemode(player, 1) || id == "minecraft:air" || player.hasTag("matrix:break-disabled")) return;
     const config = c();
     if (config.antiXray.notifyAt.some((type) => id.endsWith(type))) {
-        flag(player, "Xray", "A", 0, "none", [lang(">Block") + ":" + id, lang(">Break") + ":" + Object.values(location).join(" ")]);
+        flag(player, "Xray", "A", 0, "none", ["Block" + ":" + id, "Break" + ":" + Object.values(location).join(" ")]);
     }
 };
 
