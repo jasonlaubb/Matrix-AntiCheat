88e215e5d7ce6f96860a3ee14c44615fc477615d
jasonlaubb <jasonlau5378@gmai.com>
2024-06-27 22:13:44 +0800

    Better client auth

--- src/Modules/Movement/ClientAuth.ts
+++ src/Modules/Movement/ClientAuth.ts
@@ -1,24 +1,37 @@
-import { GameMode, PlayerSpawnAfterEvent, system, world, MinecraftEntityTypes } from "@minecraft/server";
+import { GameMode, PlayerSpawnAfterEvent, system, world } from "@minecraft/server";
 import { isAdmin, kick } from "../../Assets/Util";
 import { registerModule, configi } from "../Modules.js";
-import lang from "../../Data/Languages/lang";
+import { MinecraftEntityTypes } from "../../node_modules/@minecraft/vanilla-data/lib/index";
 
 /**
  * @author jasonlaubb
  * @description A simple client authentication to detect bot client
  */
-function firstEvent(config: configi, { player, initialSpawn }: PlayerSpawnAfterEvent) {
+
+
+async function firstEvent(_config: configi, { player, initialSpawn }: PlayerSpawnAfterEvent, i = 0) {
     if (!initialSpawn || isAdmin(player) || player.getGameMode() == GameMode.spectator) return;
     // Apply a small knockback to the player
-    player.teleport({ x: player.location.x, y: player.location.y + 1, z: player.location.z });
+    const tpState = player.tryTeleport({ x: player.location.x, y: player.location.y + 1, z: player.location.z });
+    await sleep();
+    // Max 20 tries to teleport the player
+    if (i > 20) return;
+    if (!tpState) {
+        // Try again
+        firstEvent (_config, { player, initialSpawn: true }, i + 1);
+        return;
+    }
     const currentLocation = JSON.stringify({ x: player.location.x, y: player.location.y + 1, z: player.location.z });
-    system.runTimeout(() => {
-        const locationNow = JSON.stringify(player.location);
-        if (locationNow == currentLocation) {
-            kick(player, "Client authentication failed", lang(".Bot.by"));
-            console.warn(`ClientAuth :: ${player.name} >> Bot client is blocked`);
-        }
-    }, 5);
+    await sleep();
+    const locationNow = JSON.stringify(player.location);
+    if (locationNow == currentLocation) {
+        kick(player, "Client authentication failed", "Matrix AntiCheat");
+        console.warn(`ClientAuth :: ${player.name} :: Bot client is blocked`);
+    }
+}
+
+async function sleep () {
+    return new Promise<void>((resolve) => system.run(() => resolve()));
 }
 
 registerModule("clientAuth", false, [], {
