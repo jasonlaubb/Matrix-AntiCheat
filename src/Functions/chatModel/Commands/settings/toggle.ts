245347133604a74128dc056d1b4d95ee970d9e17
jasonlaubb <jasonlau5378@gmai.com>
2024-06-27 21:57:16 +0800

    Toggle command

--- src/Functions/chatModel/Commands/settings/toggle.ts
+++ src/Functions/chatModel/Commands/settings/toggle.ts
@@ -1,7 +1,7 @@
 import { c } from "../../../../Assets/Util";
 import { registerCommand, sendRawText, verifier } from "../../CommandHandler";
 import { validModules } from "../../../../Data/Help";
-import { getModuleState, keys, antiCheatModules } from "../../../../Modules/Modules";
+import { getModulesIds, intilizeModules } from "../../../../Modules/Modules";
 import Dynamic from "../../../Config/dynamic_config";
 
 registerCommand({
@@ -15,20 +15,19 @@ registerCommand({
     executor: async (player, args) => {
         const config = c();
         const prefix = config.commands.prefix;
-        if (keys.includes(args[0])) {
-            const state = getModuleState(args[0]);
+        if (getModulesIds().includes(args[0])) {
+            // const state = Dynamic.get([args[0], "enabled"]);
             if ((args[1] == "enable") == (config as any)[args[0]].enabled && args[1] != "default") return sendRawText(player, { text: "§bMatrix §7>§c " }, { translate: "toggles.already", with: [args[1]] });
             if ((config as { [key: string]: any })[args[0]].experimental && args[2] != "force" && args[1] == "enable") return sendRawText(player, { text: "§bMatrix §7>§c " }, { translate: "toggles.warning", with: [prefix, args[0]] });
             if (args[1] === "enable") {
-                antiCheatModules[args[0]].enable();
                 Dynamic.set([args[0], "enabled"], true);
+                intilizeModules();
             } else if (args[1] == "disable") {
-                antiCheatModules[args[0]].disable();
                 Dynamic.set([args[0], "enabled"], false);
+                intilizeModules();
             } else {
-                const configState = (config as { [key: string]: any })[args[0]];
-                if (state != configState) state ? antiCheatModules[args[0]].disable() : antiCheatModules[args[0]].enable();
                 Dynamic.delete([args[0], "enabled"]);
+                intilizeModules();
             }
             sendRawText(player, { text: "§bMatrix §7>§g " }, { translate: "toggles.toggleChange", with: [args[0], args[1]] });
         } else {
