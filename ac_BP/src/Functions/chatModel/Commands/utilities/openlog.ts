02ee4f113d05b8753bcd8ecec3adb7d0dcc51f2e
jasonlaubb <jasonlau5378@gmai.com>
2024-07-14 20:55:27 +0800

    Many thing

---
+++ ac_BP/src/Functions/chatModel/Commands/utilities/openlog.ts
@@ -0,0 +1,17 @@
+import { c } from "../../../../Assets/Util";
+import { registerCommand, verifier } from "../../CommandHandler";
+import { rawstr } from "../../../../Assets/Util";
+import { sendLog } from "../../../moderateModel/log";
+
+registerCommand({
+    name: "openlog",
+    description: "View the log record of the anticheats",
+    parent: false,
+    maxArgs: 0,
+    minArgs: 0,
+    require: (player) => verifier(player, c().commands.openlog),
+    executor: async (player, _args) => {
+        player.sendMessage(new rawstr(true, "g").tra("ui.closechat").parse());
+        sendLog(player);
+    }
+})
