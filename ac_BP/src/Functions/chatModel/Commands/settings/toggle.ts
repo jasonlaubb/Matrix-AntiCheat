04278d0d2a8b4de9a7d7ab1ba94a7fc258a9aa0f
jasonlaubb <jasonlau5378@gmai.com>
2024-07-15 22:52:45 +0800

    a

--- ac_BP/src/Functions/chatModel/Commands/settings/toggle.ts
+++ ac_BP/src/Functions/chatModel/Commands/settings/toggle.ts
@@ -15,8 +15,10 @@ registerCommand({
     executor: async (player, args) => {
         const config = c();
         const prefix = config.commands.prefix;
-        if ((await getValidModules()).includes(args[0])) return sendRawText(player, { text: "§bMatrix §7>§c " }, { translate: "toggles.unknown", with: [args[0]] });
-        if ((await getModulesIds()).includes(args[0])) {
+        const validmodules = await getValidModules()
+        const moduleids = await getModulesIds();
+        if (validmodules.includes(args[0])) return sendRawText(player, { text: "§bMatrix §7>§c " }, { translate: "toggles.unknown", with: [args[0]] });
+        if (moduleids.includes(args[0])) {
             // const state = Dynamic.get([args[0], "enabled"]);
             if ((args[1] == "enable") == (config as any)[args[0]].enabled && args[1] != "default") return sendRawText(player, { text: "§bMatrix §7>§c " }, { translate: "toggles.already", with: [args[1]] });
             if ((config as { [key: string]: any })[args[0]].experimental && args[2] != "force" && args[1] == "enable") return sendRawText(player, { text: "§bMatrix §7>§c " }, { translate: "toggles.warning", with: [prefix, args[0]] });
