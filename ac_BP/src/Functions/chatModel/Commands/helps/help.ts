29dd576ed3e22d9651dc4acee3e8dbdd66caa3cc
jasonlaubb <jasonlau5378@gmai.com>
2024-07-19 21:19:43 +0800

    A

--- ac_BP/src/Functions/chatModel/Commands/helps/help.ts
+++ ac_BP/src/Functions/chatModel/Commands/helps/help.ts
@@ -8,7 +8,7 @@ registerCommand({
     maxArgs: 0,
     require: (player) => verifier(player, c().commands.help),
     executor: async (player, _args) => {
-        const helps = "Sorry, this command hasn't been implemented yet. Please check back later!";
+        const helps = "<This command is not available currently>";
         sendRawText(player, { text: "§bMatrix §7>§g " }, { translate: "help.helpcdlist", with: [] }, { text: `\n${helps}` });
     },
 });
