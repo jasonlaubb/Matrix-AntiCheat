dda1285493dc24c5037242658690ab88b285c6f9
jasonlaubb <jasonlau5378@gmai.com>
2024-07-21 17:17:37 +0800

    a

---
+++ docs/cover.md
@@ -0,0 +1,8 @@
+<img src='https://raw.githubusercontent.com/jasonlaubb/Matrix-AntiCheat/main/.github/PRIVATE_DATA/title.png'></img>
+
+## Matrix AntiCheat - The best Minecraft bedrock AntiCheat solution
+
+[Get started](README.md)
+
+[Discord](https://discord.gg/CqZGXeRKPJ)
+[Download](https://github.com/jasonlaubb/Matrix-AntiCheat/releases/latest)
---
+++ docs/script.js
@@ -0,0 +1,18 @@
+//@ts-check
+window.$docsify = {
+    name: "Matrix AntiCheat",
+    repo: 'https://github.com/jasonlaubb/Matrix-AntiCheat',
+    loadSidebar: true,
+    logo: './logo.png',
+    coverpage: "cover.md",
+    onlyCover: true,
+    loadSidebar: true,
+    subMaxLevel: 2,
+    search: {
+        paths: 'auto',
+        noData: 'No Result was found.',
+        placeholder: "Search",
+        depth: 6,
+        hideOtherSidebarContent: false
+    },
+}
