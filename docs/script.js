0de555a3a941df4c1e4a13dcd89e71ce0b9d0725
jasonlaubb <136591629+jasonlaubb@users.noreply.github.com>
2024-07-21 18:14:00 +0800

    Update script.js

--- docs/script.js
+++ docs/script.js
@@ -4,6 +4,8 @@ window.$docsify = {
     el: "#app",
     repo: 'https://github.com/jasonlaubb/Matrix-AntiCheat',
     loadSidebar: true,
+    name: "Matrix AntiCheat",
     coverpage: "cover.md",
     onlyCover: true,
+    nativeEmoji: true
 }
