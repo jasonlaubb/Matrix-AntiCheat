296e5db6ba5d157588b5f75916577af466e67ff1
jasonlaubb <jasonlau5378@gmai.com>
2024-07-21 20:45:06 +0800

    aa

--- docs/script.js
+++ docs/script.js
@@ -7,9 +7,11 @@ window.$docsify = {
     name: "Matrix AntiCheat",
     hideSidebar: false,
     auto2top: true,
-    coverpage: true,
+    coverpage: "_coverpage.md",
+    notFoundPage: "_404.md",
     onlyCover: true,
     nativeEmoji: true,
     relativePath: true,
+
     themeColor: "#383838"
 }
