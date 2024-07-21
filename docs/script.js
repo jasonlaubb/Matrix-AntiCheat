668ca62d4ecbd81bc9c6a914b51f712e5e776778
jasonlaubb <jasonlau5378@gmai.com>
2024-07-21 21:05:39 +0800

    aaaaaaaaa

--- docs/script.js
+++ docs/script.js
@@ -3,15 +3,17 @@
 window.$docsify = {
     el: "#app",
     repo: 'https://github.com/jasonlaubb/Matrix-AntiCheat',
+    logo: '/images/logo.png',
     loadSidebar: true,
     name: "Matrix AntiCheat",
     hideSidebar: false,
     auto2top: true,
-    coverpage: "coverpage.md",
-    notFoundPage: "404.md",
+    coverpage: true,
+    notFoundPage: true,
     onlyCover: true,
     nativeEmoji: true,
     relativePath: true,
     themeColor: "#383838",
-    externalLinkRel: false
+    externalLinkRel: false,
+    subMaxLevel: 2,
 }
