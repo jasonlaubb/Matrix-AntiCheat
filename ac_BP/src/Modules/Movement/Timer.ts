fb4604174c0915e354a2e6f3c147cbbf555bd0a2
RaMiGamer <140818153+RaMiGamerDev@users.noreply.github.com>
2024-08-26 01:37:59 +0100

    improved effiency and prevented so much false postitves

--- ac_BP/src/Modules/Movement/Timer.ts
+++ ac_BP/src/Modules/Movement/Timer.ts
@@ -18,13 +18,14 @@ interface timerData {
     timerLog: number;
     yLog: number;
     yDisLog: number;
-    lastHighTeleport: number;
+    flagCounter: number;
 }
 const timerData = new Map<string, timerData>();
 /** @description Return that player is spike lagging */
 /**
  * @author RamiGamerDev
- * @description Checks if the player moved without the same between velocity and moved distance.
+ * @description Checks if the player moved without the same value and velocity and moved distance.*
+ * @credit jason cleaned the code && orange cat helped preventing false positive
  */
 export async function AntiTimer(config: configi, player: Player, now: number) {
     const data = timerData.get(player.id) ?? ({} as timerData);
@@ -38,13 +39,17 @@ export async function AntiTimer(config: configi, player: Player, now: number) {
     //check if dBVD lower than 1 and higher than 0.5 add one to timerLog and when timerLog reach 3 flag (check for low timer)
     if ((dBVD < data.maxDBVD && dBVD > 20 / (tps.getTps()! * 2)) || (dBVD2 < data.maxDBVD && dBVD2 > 20 / (tps.getTps()! * 2))) data.timerLog++;
     else data.timerLog = 0;
-    if (!bypassMovementCheck(player) && now - data.lastHighTeleport >= config.antiTimer.tickMovementCooldown && (((dBVD > data.maxDBVD || dBVD2 > data.maxDBVD) && now - data.lastFlag >= 1025) || data.timerLog >= config.antiTimer.minTimerLog)) {
+    if (!bypassMovementCheck(player) && (((dBVD > data.maxDBVD || dBVD2 > data.maxDBVD) && now - data.lastFlag >= 1025) || data.timerLog >= config.antiTimer.minTimerLog)) {
         //dBLFN = difference between last flag time and now
         const dBLFN = now - data.lastFlag;
+        //counting how many times did the player got detected in 10 seconds
+        if (dBLFN <= 10000) data.flagCounter =+ 1
+        else data.flagCounter = 0
         //if the dBLFN is lower than the given value flag
-        if (getMsPerTick() > 42 && !isSpikeLagging(player) && ((dBLFN < 5000 && data.timerLog >= 3) || (dBLFN < 2000 && dBVD > data.maxDBVD)))
+        if (getMsPerTick() > 42 && !isSpikeLagging(player) && ((data.flagCounter > 2 && timerLog[player.id] >= 3 ) || (data.flagCounter> 5 && dBVD > maxDBVD[player.id])))
             flag(player, "Timer", "A", config.antiTimer.maxVL, config.antiTimer.punishment, ["blockPerSecond" + ":" + (data.disLog * 2).toFixed(2)]);
         //lag back the player
+        if (dBVD >= 3 || data.flagCounter >= 3)
         freezeTeleport(player, data.safeZone);
         //setting new lastFlag
         data.lastFlag = now;
@@ -64,7 +69,6 @@ export async function SystemEvent(player: Player, now: number) {
     const locdata = data.locationData ?? { location: player.location, recordTime: now };
     //skip the code for for some reasons
     data.locationData = { location: player.location, recordTime: now };
-    data.lastHighTeleport ??= 0;
     data.lastTickPos ??= player.location;
     const distance = Math.hypot(player.location.x - data.lastTickPos.x, player.location.z - data.lastTickPos.z);
     data.lastTickPos = player.location;
