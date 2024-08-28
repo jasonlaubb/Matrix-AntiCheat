52773005f7aa253493442bd737b6549e3ab6a31a
jasonlaubb <jasonlau5378@gmai.com>
2024-08-28 10:18:18 +0800

    Update the builder

--- matrix-builder.bat
+++ matrix-builder.bat
@@ -1,36 +1,3 @@
-::[Bat To Exe Converter]
-::
-::YAwzoRdxOk+EWAnk
-::fBw5plQjdG8=
-::YAwzuBVtJxjWCl3EqQJgSA==
-::ZR4luwNxJguZRRnk
-::Yhs/ulQjdF+5
-::cxAkpRVqdFKZSDk=
-::cBs/ulQjdF+5
-::ZR41oxFsdFKZSDk=
-::eBoioBt6dFKZSDk=
-::cRo6pxp7LAbNWATEpCI=
-::egkzugNsPRvcWATEpCI=
-::dAsiuh18IRvcCxnZtBJQ
-::cRYluBh/LU+EWAnk
-::YxY4rhs+aU+JeA==
-::cxY6rQJ7JhzQF1fEqQJQ
-::ZQ05rAF9IBncCkqN+0xwdVs0
-::ZQ05rAF9IAHYFVzEqQJQ
-::eg0/rx1wNQPfEVWB+kM9LVsJDGQ=
-::fBEirQZwNQPfEVWB+kM9LVsJDGQ=
-::cRolqwZ3JBvQF1fEqQJQ
-::dhA7uBVwLU+EWDk=
-::YQ03rBFzNR3SWATElA==
-::dhAmsQZ3MwfNWATElA==
-::ZQ0/vhVqMQ3MEVWAtB9wSA==
-::Zg8zqx1/OA3MEVWAtB9wSA==
-::dhA7pRFwIByZRRnk
-::Zh4grVQjdCyDJGyX8VAjFDFVXwuMD16JOpET6/326uSTsXFTeeMraobD5pmPNPIa8gjle4Ik6ndbjNkFFFZaaxunagom52taswQ=
-::YB416Ek+ZG8=
-::
-::
-::978f952a14a936cc963da21a135fa983
 @echo off
 echo Please check if you have installed NodeJS. It's required when building.
 echo This anticheat project is under AGPL 3.0 license. Please follow it.
@@ -42,6 +9,7 @@ if /i "%user_input%" neq "Y" (
     exit /b
 )
 : Install the node modules
+call npm install -g typescript
 call npm install
 call npm install --prefix ./ac_BP/src
 if exist "%~dp0ac_BP\src\node_modules\matrix-anticheat" rmdir /S /Q "%~dp0ac_BP\src\node_modules\matrix-anticheat"
