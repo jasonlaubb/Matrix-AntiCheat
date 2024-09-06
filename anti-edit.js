bb139636948efd5772563ed650fcec9c30147db2
jasonlaubb <jasonlau5378@gmai.com>
2024-09-06 23:18:21 +0800

    Ignore the Data

--- anti-edit.js
+++ anti-edit.js
@@ -4,16 +4,27 @@ import obfuscator from 'javascript-obfuscator';
 import progressBar from 'progress';
 
 const directoryPath = './generated-package/Matrix-anti_BP/scripts';
-
+const whiteList = [
+  'Default.js',
+  'Config.js',
+  'ChatFilterData.js',
+  'RegexBasedFilter.js',
+  'FastBrokenBlocks.js',
+  'EducationItemList.js',
+  'EnumData.js',
+  'FastBrokenBlocks.js',
+  'OperatorItemList.js',
+  'ValidPlayerProperty.js',
+]
 async function traverseDirectory(directoryPath) {
   const files = await fs.promises.readdir(directoryPath, { withFileTypes: true });
 
   for (const file of files) {
     const filePath = path.join(directoryPath, file.name);
     if (file.isDirectory()) {
-      if (file.path.includes('node_modules') || file.path.includes('Data')) continue;
+      if (file.name.includes('node_modules')) continue;
       await traverseDirectory(filePath);
-    } else if (file.isFile() && file.name.endsWith('.js')) {
+    } else if (file.isFile() && file.name.endsWith('.js') && !whiteList.includes(file.name)) {
       bar.tick({ file: file.name });
       bar.render();
       await obfuscateFile(filePath);
@@ -63,9 +74,9 @@ async function countLoopIterations(directoryPath) {
     const filePath = path.join(directoryPath, file.name);
 
     if (file.isDirectory()) {
-      if (file.path.includes('node_modules') || file.path.includes('Data')) continue;
+      if (file.name.includes('node_modules')) continue;
       count += await countLoopIterations(filePath);
-    } else if (file.isFile() && file.name.endsWith('.js')) {
+    } else if (file.isFile() && file.name.endsWith('.js') && !whiteList.includes(file.name)) {
       count++;
     }
   }
