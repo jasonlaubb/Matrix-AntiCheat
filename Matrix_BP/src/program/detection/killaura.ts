1feb2cbc8adee4cc5a03de72c327ab18ec0cbaeb
jasonlaubb <jasonlau5378@gmai.com>
2024-11-23 08:20:38 +0800

    Add the round killaura check

--- Matrix_BP/src/program/detection/killaura.ts
+++ Matrix_BP/src/program/detection/killaura.ts
@@ -1,11 +1,12 @@
-import { EntityHitEntityAfterEvent, Player, world } from "@minecraft/server";
+import { EntityHitEntityAfterEvent, Player, Vector2, world } from "@minecraft/server";
 import { IntegratedSystemEvent, Module } from "../../matrixAPI";
 import { rawtextTranslate } from "../../util/rawtext";
-import { calculateAngleFromView, calculateDistance, fastAbs } from "../../util/fastmath";
+import { calculateAngleFromView, calculateDistance, fastAbs, fastRound } from "../../util/fastmath";
 import { getAngleLimit } from "../../util/util";
 const KILLAURA_DISTANCE_THRESHOLD = 3.5;
 const KILLAURA_PVP_DISTANCE_THRESHOLD = 4.5;
 const KILLAURA_ROTATION_THRESHOLD = 79;
+const MIN_ROUND_DIFFERENCE = 0.07;
 
 let eventId: IntegratedSystemEvent;
 const killaura = new Module()
@@ -25,8 +26,13 @@ const killaura = new Module()
     });
 
 killaura.register();
-
-const killauraData = new Map<string, string[]>();
+interface killAuraData {
+    entityHurtList: string[];
+    roundFlagAmount: number;
+    lastAttackRot: Vector2;
+    lastRoundTimestamp: number;
+}
+const killauraData = new Map<string, killAuraData>();
 
 /**
  * @author jasonlaubb
@@ -41,15 +47,15 @@ function entityHitEntity({ damagingEntity: player, hitEntity: target }: EntityHi
 
     const distance = calculateDistance(player.location, target.location);
     const isPvp = target instanceof Player;
-    const rotationY = player.getRotation().y;
+    const { x: pitch, y: yaw } = player.getRotation();
 
-    if (isPvp && distance > KILLAURA_PVP_DISTANCE_THRESHOLD && fastAbs(rotationY) > KILLAURA_ROTATION_THRESHOLD) {
+    if (isPvp && distance > KILLAURA_PVP_DISTANCE_THRESHOLD && fastAbs(yaw) > KILLAURA_ROTATION_THRESHOLD) {
         player.flag(killaura);
         return;
     }
 
     if (distance > KILLAURA_DISTANCE_THRESHOLD && isPvp) {
-        const angle = calculateAngleFromView(player.location, target.location, rotationY);
+        const angle = calculateAngleFromView(player.location, target.location, yaw);
         const angleLimit = getAngleLimit(player.clientSystemInfo.platformType);
         if (angle > angleLimit) {
             player.flag(killaura);
@@ -57,14 +63,39 @@ function entityHitEntity({ damagingEntity: player, hitEntity: target }: EntityHi
         }
     }
 
-    const data = killauraData.get(player.id) ?? [];
-    if (data.includes(target.id)) data.push(target.id);
-    killauraData.set(player.id, data);
-    if (data.length >= 3) {
+    const data = killauraData.get(player.id)!;
+    if (data.entityHurtList.includes(target.id)) data.entityHurtList.push(target.id);
+    if (data.entityHurtList.length >= 3) {
         player.flag(killaura);
     }
+    // Checks if the player has integer pitch or yaw.
+    const isNotTeleportYaw = yaw != 0;
+    const isNotTeleportPitch = pitch != 0;
+    if ((Number.isInteger(pitch) && isNotTeleportPitch) || (Number.isInteger(yaw) && isNotTeleportYaw)) {
+        player.flag(killaura);
+    } else {
+        const intRot = fastRound(yaw);
+        const intPitch = fastRound(pitch);
+        const yawDifferent = fastAbs(yaw - intRot);
+        const pitchDifferent = fastAbs(pitch - intPitch);
+        if (data.lastAttackRot.x != pitch && data.lastAttackRot.y != yaw && (yawDifferent < MIN_ROUND_DIFFERENCE && isNotTeleportYaw) || (pitchDifferent < MIN_ROUND_DIFFERENCE && isNotTeleportPitch)) {
+            const now = Date.now();
+            if (now - data.lastRoundTimestamp > 7000) {
+                data.roundFlagAmount = 0;
+            }
+            data.roundFlagAmount++;
+            data.lastRoundTimestamp = now;
+            if (data.roundFlagAmount >= 3) {
+                player.flag(killaura);
+            }
+        }
+    }
+    data.lastAttackRot = { x: pitch, y: yaw } as Vector2;
+    killauraData.set(player.id, data);
 }
 
 function tickEvent(player: Player) {
-    killauraData.set(player.id, []);
+    const data = killauraData.get(player.id)!;
+    data.entityHurtList = [];
+    killauraData.set(player.id, data);
 }
--- Matrix_BP/src/util/fastmath.ts
+++ Matrix_BP/src/util/fastmath.ts
@@ -10,30 +10,6 @@ export function calculateAngleFromView(pos1: Vector3, pos2: Vector3, rotationY: 
     const finalAngle = rotatedAngle <= -180 ? rotatedAngle + 360 : rotatedAngle;
     return fastAbs(finalAngle);
 }
-/*
-export function fastAtan2(y: number, x: number) {
-    //const absY = fastAbs(y) + 1e-10;
-    const angle = Math.atan(y / x);
-    if (x >= 0) return angle;
-    else return y >= 0 ? angle + PI : angle - PI;
-}
-
-export function fastAtan(x: number) {
-    const a1 = 0.99997726;
-    const a3 = -0.33262347;
-    const a5 = 0.19354346;
-    const a7 = -0.11643287;
-    const a9 = 0.05265332;
-    const a11 = -0.01172120;
-
-    const x2 = x * x;
-    const x4 = x2 * x2;
-    const x6 = x4 * x2;
-    const x8 = x6 * x2;
-    const x10 = x8 * x2;
-
-    return x * (a1 + a3 * x2 + a5 * x4 + a7 * x6 + a9 * x8 + a11 * x10);
-}*/
 
 // Distance functions
 export function calculateDistance(pos1: Vector3, pos2: Vector3): number {
@@ -43,6 +19,10 @@ export function calculateDistance(pos1: Vector3, pos2: Vector3): number {
 // General Maths functions
 export function fastRound(x: number) {
     return (x + 0.5) | 0;
+}
+
+export function fastTrunc(x: number) {
+    return x | 0;
 }
 
 export function fastAbs(x: number) {
