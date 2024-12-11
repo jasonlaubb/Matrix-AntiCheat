3e18e4bf3c0141e2481730cf2c457ddbe830784e
jasonlaubb <jasonlau5378@gmai.com>
2024-12-11 17:59:15 +0800

    qwq

--- Matrix_BP/src/program/detection/reach.ts
+++ Matrix_BP/src/program/detection/reach.ts
@@ -1,6 +1,5 @@
 import { EntityHitEntityAfterEvent, Player, Vector3, system, world } from "@minecraft/server";
 import { fastCos, fastHypot, fastAbs } from "../../util/fastmath";
-import { fastAbs } from "../../util/fastmath";
 import { Module } from "../../matrixAPI";
 import { rawtextTranslate } from "../../util/rawtext";
 const reach = new Module()
