3ef4ea4a721514b7dc6284f7ca6c708f74d0f146
jasonlaubb <jasonlau5378@gmai.com>
2025-03-09 20:22:34 +0800

    eew

--- Matrix_BP/src/assets/itemcheck.ts
+++ Matrix_BP/src/assets/itemcheck.ts
@@ -8,9 +8,9 @@ const legit = new Set(Object.values(MinecraftItemTypes) as string[]);
  */
 export function isItemIllegal(item: ItemStack) {
     const itemID = item.typeId;
-    return legit.has(itemID) && item.amount <= item.maxAmount && item.amount >= 0 && (item.nameTag?.length ?? 0) <= 256 && item.getLore().length == 0 && !isLegalEnchantment(item);
+    return legit.has(itemID) && item.amount <= item.maxAmount && item.amount >= 0 && (item.nameTag?.length ?? 0) <= 256 && item.getLore().length == 0 && !hasillegalEnchantment(item);
 }
-function isLegalEnchantment(item: ItemStack) {
+function hasillegalEnchantment(item: ItemStack) {
     const enchantmentComponent = item.getComponent("enchantable");
     if (!enchantmentComponent) return true;
     const enchantments = enchantmentComponent.getEnchantments();
