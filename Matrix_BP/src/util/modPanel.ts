2aca58267b1a5c7b1f03ffe94bf1ba79f132a05e
jasonlaubb <jasonlau5378@gmai.com>
2025-03-09 15:55:17 +0800

    ewe

--- Matrix_BP/src/util/modPanel.ts
+++ Matrix_BP/src/util/modPanel.ts
@@ -8,7 +8,7 @@ export class ModPanel {
         if (!player) {
             const allPlayers = world.getAllPlayers().map(({ name }) => name);
             const res = await new ModalFormData()
-                .title(rawtextTranslate("ui.modpanel.sel.title"))
+                .title(rawtextTranslate("ui.modpanel.title"))
                 .dropdown(rawtextTranslate("ui.modpamel.sel.here"), allPlayers)
                 //@ts-expect-error
                 .show(admin);
@@ -17,7 +17,32 @@ export class ModPanel {
         } else {
             playerName = player.name;
         }
-        new ActionFormData()
-
+        const res = await new ActionFormData()
+            .title(rawtextTranslate("ui.modpanel.title", playerName))
+            .button(`§6MUTE\n§8Run '-mute ${playerName} 15'`)
+            .button(`§cKICK\n§8Run '-kick ${playerName}'`)
+            .button(`§4BAN\n§8Run '-ban ${playerName}'`)
+            .button(`§1CRASH\n§8Run '-crash ${playerName}'`)
+            //@ts-expect-error
+            .show(admin);
+        if (res.canceled) return;
+        switch (res.selection) {
+            case 0: {
+                admin.runChatCommand(`mute ${playerName} 15`);
+                break;
+            }
+            case 1: {
+                admin.runChatCommand(`kick ${playerName}`);
+                break;
+            }
+            case 2: {
+                admin.runChatCommand(`ban ${playerName}`);
+                break;
+            }
+            case 3: {
+                admin.runChatCommand(`crash ${playerName}`);
+                break;
+            }
+        }
     }
 }
