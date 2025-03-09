3e9c76dd8a0b01dcc3ea6e789148b518fef57e92
jasonlaubb <jasonlau5378@gmai.com>
2025-03-09 21:49:52 +0800

    Part...

--- Matrix_BP/src/program/command/mset.ts
+++ Matrix_BP/src/program/command/mset.ts
@@ -5,30 +5,35 @@ import { ModalFormData } from "@minecraft/server-ui";
 import { waitShowModalForm } from "../../util/util";
 const MATCH_REG = /#[(a-zA-Z)|/]+\,[^#,]+#/g
 const TEST_REG = /^(#[(a-zA-Z)|/]+\,[^#,]+#)+$/
+let part: { [key: string]: number } = {};
 new Command()
 	.setDescription(rawtextTranslate("command.mset.description"))
 	.setName("mset")
 	.setMinPermissionLevel(3)
 	.setAliases("multiset", "fastset", "fset", "import")
 	.onExecute(async (player) => {
-		const data = await waitShowModalForm(new ModalFormData()
-			.title(rawtextTranslate("command.mset.title"))
-			.textField(rawtextTranslate("command.mset.input"), "<key here>")
-			.submitButton("Press to input"), player);
-		if (data === null || data.canceled) return;
-		const key = data.formValues![0] as string;
-		const match = key.match(MATCH_REG);
-		if (match === null || TEST_REG.test(key) === false) {
-			player.sendMessage(fastText().addText("§bMatrix§a+ §7> §c").addTran("command.mset.error").build());
-			return;
-		}
-		match.forEach((value) => {
-			const [key, ...nv] = value.slice(1, -1).split(",");
-			player.runChatCommand(`set ${key} "${nv.join(",")}"`)
-		});
-		player.sendMessage(fastText().addText("§bMatrix§a+ §7> §g").addTran("command.mset.success", match.length.toString()).build());
+		await loop(player);
 	})
 	.register();
+async function loop (player: Player, i = 1) {
+	const data = await waitShowModalForm(new ModalFormData()
+		.title(rawtextTranslate("command.mset.title"))
+		.textField(rawtextTranslate("command.mset.input"), "<key here>")
+		.submitButton("Import part " + i), player);
+	if (data === null || data.canceled) return;
+	const key = data.formValues![0] as string;
+	const match = key.match(MATCH_REG);
+	if (match === null || TEST_REG.test(key) === false) {
+		player.sendMessage(fastText().addText("§bMatrix§a+ §7> §c").addTran("command.mset.error").build());
+		return;
+	}
+	match.forEach((value) => {
+		const [key, ...nv] = value.slice(1, -1).split(",");
+		player.runChatCommand(`set ${key} "${nv.join(",")}"`)
+	});
+	player.sendMessage(fastText().addText("§bMatrix§a+ §7> §g").addTran("command.mset.success", match.length.toString()).build());
+	loop(player, i + 1);
+}
 new Command()
 	.setDescription(rawtextTranslate("command.export.description"))
 	.setName("export")
--- docs/gen.js
+++ docs/gen.js
@@ -1,3 +1,5 @@
+var output = [];
+var currentPart = 1;
 function generateKey () {
 	let input = document.getElementById("textbox").value;
 	input = input.replace("export default", "").trim();
@@ -17,11 +19,15 @@ function generateKey () {
 		return;
 	}
 	const converted = convertJson(parsed);
-	const output = Object.entries(converted).map(([key, value]) => {
+	const outputt = Object.entries(converted).map(([key, value]) => {
 		return `#${key},${typeof value === "boolean" ? (value ? "true" : "false") : value.toString()}#`;
 	}).join("").replace(/#[(a-zA-Z)|/]+\,#/g, "");
-	document.getElementById("output").innerHTML = output;
 	document.getElementById("textbox").value = "";
+	currentPart = 1;
+	console.log(truncateString(outputt));
+	output = truncateString(outputt);
+	document.getElementById("output").innerHTML = output[0];
+	document.getElementById("copy-btn").innerHTML = `Copy (Part 1 of ${output.length})`;
 }
 
 function convertJson(obj, prefix = '') {
@@ -43,10 +49,30 @@ function stringToJson(str) {
 	return JSON.parse(str.replace(/(\w+):/g, '"$1":').replace(/,[\s]*}/g, "}"));
 }
 function copyText() {
-	var output = document.getElementById("output").textContent;
-	navigator.clipboard.writeText(output).then(function() {
+	if (output.length === 0) return;
+	navigator.clipboard.writeText(output[currentPart - 1]).then(function() {
 	  console.log("Text copied to clipboard");
 	}, function(err) {
 	  console.error("Could not copy text: ", err);
 	});
+	currentPart++;
+	if (currentPart > output.length) currentPart = 1;
+	document.getElementById("copy-btn").innerHTML = `Copy (Part ${currentPart} of ${output.length})`;
+	document.getElementById("output").innerHTML = output[currentPart - 1];
+}
+function truncateString(s) {
+	const result = [];
+	const regex = /#[(a-zA-Z)|/]+\,[^#]+#/g;
+	const item = s.match(regex);
+	let itemStr = "";
+	for (const str of item) {
+		if (itemStr.length + str.length <= 100) {
+			itemStr += str;
+		} else {
+			result.push(itemStr);
+			itemStr = "";
+		}
+	}
+	if (itemStr.length > 0) result.push(itemStr);
+	return result;
 }
