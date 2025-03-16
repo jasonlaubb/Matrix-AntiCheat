abf1e08a30a238c1156946fa0d3ccbe148f7ecf6
jasonlaubb <jasonlau5378@gmai.com>
2025-03-16 16:04:59 +0800

    Support more type

--- Matrix_BP/src/assets/customComponentRegistry.ts
+++ Matrix_BP/src/assets/customComponentRegistry.ts
@@ -1,7 +1,7 @@
-import { BlockComponentPlayerDestroyEvent, BlockCustomComponent, ItemComponentHitEntityEvent, ItemComponentUseEvent, ItemCustomComponent, ItemStack, Player, world } from "@minecraft/server";
-import { noDrop, randomInt, spawnExpOrbs, vanillaAny, weightRandom } from "../util/util";
-import { MinecraftEnchantmentTypes, MinecraftItemTypes } from "../node_modules/@minecraft/vanilla-data/lib/index";
+import { ItemComponentHitEntityEvent, ItemComponentUseEvent, ItemCustomComponent, Player, world } from "@minecraft/server";
+import { MinecraftItemTypes } from "../node_modules/@minecraft/vanilla-data/lib/index";
 import { ModPanel } from "../util/modPanel";
+import tile from "../util/tileBuilder";
 class matrixui implements ItemCustomComponent {
 	constructor () {
 		this.onUse = this.onUse.bind(this);
@@ -23,56 +23,13 @@ class modPanel implements ItemCustomComponent {
 			ModPanel.open(attackingEntity, hitEntity);
 	}
 }
-class stoneLoot implements BlockCustomComponent {
-	constructor () {
-		this.onPlayerDestroy = this.onPlayerDestroy.bind(this);
-	}
-	onPlayerDestroy ({ player, block: { location, dimension } }: BlockComponentPlayerDestroyEvent) {
-		const item = player?.getHeldItem();
-		if (player)
-			if (!item || !vanillaAny(item.typeId, "pickaxe")) return;
-		if (item && item.getEnchantLevel(MinecraftEnchantmentTypes.SilkTouch) > 0)
-			dimension.spawnItem(new ItemStack(MinecraftItemTypes.Stone, 1), location);
-		else;
-			dimension.spawnItem(new ItemStack(MinecraftItemTypes.Cobblestone, 1), location);
-	}
-}
-class diamondLoot implements BlockCustomComponent {
-	constructor () {
-		this.onPlayerDestroy = this.onPlayerDestroy.bind(this);
-	}
-	onPlayerDestroy ({ player, block: { location, dimension }, destroyedBlockPermutation: { type: { id }} }: BlockComponentPlayerDestroyEvent) {
-		if (noDrop()) return;
-		const item = player?.getHeldItem();
-		const level = (item?.getEnchantLevel(MinecraftEnchantmentTypes.Fortune) ?? 0) + 1;
-		if (player) {
-			if (!item || !vanillaAny(item.typeId, "iron_pickaxe", "diamond_pickaxe", "netherite_pickaxe")) return;
-			spawnExpOrbs(dimension, location, randomInt(3, 7), level);
-		}
-		if (item && item.getEnchantLevel(MinecraftEnchantmentTypes.SilkTouch) > 0) {
-			if (id.includes("deepslate"))
-				dimension.spawnItem(new ItemStack(MinecraftItemTypes.DeepslateDiamondOre, 1), location);
-			else;
-				dimension.spawnItem(new ItemStack(MinecraftItemTypes.DiamondOre, 1), location);
-		} else {
-			dimension.spawnItem(new ItemStack(MinecraftItemTypes.Diamond, diamondDrop(level)), location);
-		}
-	}
-}
-function diamondDrop (fortune: number = 0) {
-	if (fortune === 0) return 1;
-	const weightList: [number, number][] = [[1, 2]];
-	for (let i = 0; i < fortune; i++) {
-		weightList.push([fortune + 1, 2]);
-	}
-	return weightRandom(...weightList);
-}
 // Register the custom components
 world.beforeEvents.worldInitialize.subscribe((init) => {
 	// Matrix Items
 	init.itemComponentRegistry.registerCustomComponent("matrixui", new matrixui());
 	init.itemComponentRegistry.registerCustomComponent("modPanel", new modPanel());
 	// Pretender Blocks
-	init.blockComponentRegistry.registerCustomComponent("stoneLoot", new stoneLoot());
-	init.blockComponentRegistry.registerCustomComponent("diamondLoot", new diamondLoot());
+	init.blockComponentRegistry.registerCustomComponent("stoneLoot", tile(MinecraftItemTypes.Stone, null, MinecraftItemTypes.Cobblestone, ["pickaxe"], null));
+	init.blockComponentRegistry.registerCustomComponent("diamondLoot", tile(MinecraftItemTypes.DiamondOre, MinecraftItemTypes.DeepslateDiamondOre, MinecraftItemTypes.Diamond, ["iron_pickaxe", "diamond_pickaxe", "netherite_pickaxe"], [1, 1], 3, 7));
+	init.blockComponentRegistry.registerCustomComponent("coalLoot", tile(MinecraftItemTypes.CoalOre, MinecraftItemTypes.DeepslateCoalOre, MinecraftItemTypes.Coal, ["pickaxe"], [1, 1], 0, 2));
 })
---
+++ Matrix_BP/src/util/tileBuilder.ts
@@ -0,0 +1,45 @@
+import { BlockComponentPlayerDestroyEvent, BlockCustomComponent, ItemStack } from "@minecraft/server";
+import { noDrop, randomInt, spawnExpOrbs, vanillaAny, weightRandom } from "./util";
+import { MinecraftEnchantmentTypes } from "@minecraft/vanilla-data";
+/**
+ * @author jasonlaubb
+ * @description Create custom ore drops
+ */
+export default function tileBuilder (silkTile: string, deepTile: string | null, dropItem: string, allowTool: string[], baseRange: null | [number, number], minOrbs: number = 0, maxOrbs: number = 0) {
+	class OreTile implements BlockCustomComponent {
+		constructor () {
+			this.onPlayerDestroy = this.onPlayerDestroy.bind(this);
+		}
+		onPlayerDestroy ({ player, block: { location, dimension }, destroyedBlockPermutation: { type: { id }} }: BlockComponentPlayerDestroyEvent) {
+			if (noDrop()) return;
+			const item = player?.getHeldItem();
+			const level = (item?.getEnchantLevel(MinecraftEnchantmentTypes.Fortune) ?? 0) + 1;
+			if (player) {
+				if (!item || !vanillaAny(item.typeId, ...allowTool)) return;
+				if (maxOrbs > 0) spawnExpOrbs(dimension, location, randomInt(minOrbs, maxOrbs), level);
+			}
+			if (item && item.getEnchantLevel(MinecraftEnchantmentTypes.SilkTouch) > 0) {
+				if (deepTile && id.includes("deepslate"))
+					dimension.spawnItem(new ItemStack(deepTile, 1), location);
+				else;
+					dimension.spawnItem(new ItemStack(silkTile, 1), location);
+			} else {
+				if (baseRange) {
+					dimension.spawnItem(new ItemStack(dropItem, tileMultiplier(level, baseRange)), location);
+				} else {
+					dimension.spawnItem(new ItemStack(dropItem, 1), location);
+				}
+			}
+		}
+	}
+	return new OreTile();
+}
+function tileMultiplier (fortune: number = 0, baseRange: [number, number]) {
+	if (fortune === 0) return 1;
+	const weightList: [number, number][] = [[randomInt(...baseRange), 2]];
+	for (let i = 0; i < fortune; i++) {
+		const level = i + 1;
+		weightList.push([randomInt(baseRange[0] * level, baseRange[1] * level), 1]);
+	}
+	return weightRandom(...weightList);
+}
