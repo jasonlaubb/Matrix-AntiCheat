c5eb40ae65922f358a1a71100797c37e5fdfdb41
jasonlaubb <136591629+jasonlaubb@users.noreply.github.com>
2025-03-18 22:31:33 +0800

    Update realmsplus.ts

--- Matrix_BP/src/util/realmsplus.ts
+++ Matrix_BP/src/util/realmsplus.ts
@@ -4,9 +4,8 @@ const botTag = "realmsplus";
 /** @author NoVa Gh0ul */
 export function outboundEvent (packet: any) {
         const cleanedPacket = JSON.stringify(packet).replace(/\\/g, '\\\\').replace(/"/g, '\\"');
-        world.getDimension("overworld").runCommandAsync(`tellraw @a[tag=${botTag}] {"rawtext":[{"text":"${cleanedPacket}"}]}`)
-        .catch((e) => {  });
-};
+        world.getDimension("overworld").runCommandAsync(`tellraw @a[tag=${botTag}] {"rawtext":[{"text":"${cleanedPacket}"}]}`).catch((e) => {  });
+}
 export function useRealmsPlus (
         return !Module.config.unfollowRealmsPunishment && world.getPlayers({ tags: [botTag] }).length > 0;
-};
+}
