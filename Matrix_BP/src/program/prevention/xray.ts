cdc11f68451d9024a782409a28df5e3ea8855921
jasonlaubb <jasonlau5378@gmai.com>
2025-03-22 16:19:19 +0800

    Finish

--- Matrix_BP/src/program/prevention/xray.ts
+++ Matrix_BP/src/program/prevention/xray.ts
@@ -75,7 +75,6 @@ function replaceArea(dimension: Dimension, location: Vector3, upHeight: number, 
 	const minLocation = { x: location.x - horizontal, y: fastMin(HIGHEST_Y, fastMax(LOWEST_Y, location.y - downHeight)), z: location.z - horizontal };
 	const maxLocation = { x: location.x + horizontal, y: fastMax(LOWEST_Y, fastMin(HIGHEST_Y, location.y + upHeight)), z: location.z + horizontal };
 	const volume = new BlockVolume(minLocation, maxLocation);
-	world.sendMessage(JSON.stringify(entries));
 	function* replaceArea (): Generator<void, void, void> {
 		for (const [replaceId, withId] of entries) {
 			dimension.fillBlocks(volume, withId, {
