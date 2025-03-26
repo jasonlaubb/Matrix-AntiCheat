53d71d769174c20217026d3376fc0ad0e392399e
jasonlaubb <136591629+jasonlaubb@users.noreply.github.com>
2025-03-26 17:13:20 +0800

    Create category.ts

---
+++ Matrix_BP/src/data/category.ts
@@ -0,0 +1,48 @@
+export const cmdType = [
+    {
+        name: "type.moderation",
+        icon: "",
+    },
+    {
+        name: "type.log",
+        icon: "",
+    },
+    {
+        name: "type.setting",
+        icon: "",
+    },
+    {
+        name: "type.util",
+        icon: "",
+    },
+    {
+        name: "type.other",
+        icon: "",
+    },
+];
+export const mdlType = [
+    {
+        name: "type.movement",
+        icon: "",
+    },
+    {
+        name: "type.combat",
+        icon: "",
+    },
+    {
+        name: "type.block",
+        icon: "",
+    },
+    {
+        name: "type.data",
+        icon: "",
+    },
+    {
+        name: "type.util",
+        icon: "",
+    },
+    {
+        name: "type.other",
+        icon: "",
+    },
+];
