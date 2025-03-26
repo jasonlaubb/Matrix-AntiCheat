65af0fca2ee41e0ee72dd606ee09508b70f9ea45
jasonlaubb <jasonlau5378@gmai.com>
2025-03-26 19:30:35 +0800

    Add icon

--- Matrix_BP/src/data/category.ts
+++ Matrix_BP/src/data/category.ts
@@ -1,52 +1,52 @@
 export const cmdType = [
     {
         name: "type.moderation",
-        icon: "",
+        icon: "textures/items/mace.png",
     },
     {
         name: "type.log",
-        icon: "",
+        icon: "textures/items/paper.png",
     },
     {
         name: "type.setting",
-        icon: "",
+        icon: "textures/ui/gear.png",
     },
     {
         name: "type.util",
-        icon: "",
+        icon: "textures/items/diamond.png",
     },
     {
         name: "type.security",
-        icon: "",
+        icon: "textures/items/iron_chestplate.png",
     },
     {
         name: "type.other",
-        icon: "",
+        icon: "textures/blocks/dirt.png",
     },
 ];
 export const mdlType = [
     {
         name: "type.movement",
-        icon: "",
+        icon: "textures/ui/speed_effect.png",
     },
     {
         name: "type.combat",
-        icon: "",
+        icon: "textures/items/iron_sword.png",
     },
     {
         name: "type.block",
-        icon: "",
+        icon: "textures/blocks/bedrock.png",
     },
     {
         name: "type.data",
-        icon: "",
+        icon: "textures/items/paper.png",
     },
     {
         name: "type.util",
-        icon: "",
+        icon: "textures/items/diamond.png",
     },
     {
         name: "type.other",
-        icon: "",
+        icon: "textures/blocks/dirt.png",
     },
 ];
