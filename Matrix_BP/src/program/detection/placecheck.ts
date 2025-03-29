8746998529e4bb8d189c17220c8421c784774988
jasonlaubb <136591629+jasonlaubb@users.noreply.github.com>
2025-03-29 13:48:57 +0800

    Create placecheck.ts

---
+++ Matrix_BP/src/program/detection/placecheck.ts
@@ -0,0 +1,37 @@
+import itemcheck from "./itemcheck";
+import { Module } from "../../matrixAPI";
+import { GameMode, rawtextTranslate, world, PlayerPlaceBlockAfterEvent } from "../../util/rawtext";
+
+const placeCheck = new Module()
+    .setTag(I forgot)
+    .setName(rawtextTranslate("module.placecheck.name"))
+    .setDescription(rawtextTranslate("module.placecheck.description"))
+    .onModuleEnable(() => {
+        world.afterEvents.playerPlaceBlock.subscribe(onPlace);
+    })
+    .onModuleDisable(() => {
+        world.afterEvents.playerPlaceBlock.subscribe(onPlace);
+    });
+placeCheck.register();
+
+function onPlace({ player, block }: PlayerPlaceBlockAfterEvent) {
+    if (player.isAdmin() || block.isAir || !block.isValid) return;
+    const container = block.getComponent("container")?.container;
+    if (container) {
+        let isFlagged = false;
+        for (let i = 0; i < container.size; i++) {
+            const item = container.getItem(i);
+            if (!item) continue;
+            const result = itemcheck(item);
+            if (result) {
+                if (!isFlagged) {
+                    player.flag(placeCheck, result as any);
+                    isFlagged = true;
+                }
+                container.setItem(i);
+            }
+        }
+    } else if (block.typeId.endsWith("sign")) system.runTimeout(() => {
+        const sign = block.getComponent("sign");
+    }, 1);
+}
