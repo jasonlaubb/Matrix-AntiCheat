e75af20be01ffc1c454f73b8b686ff1bb33e7556
jasonlaubb <jasonlau5378@gmail.com>
2025-06-28 16:43:47 +0800

    Easiest command

--- bp/src/command/info.ts
+++ bp/src/command/info.ts
@@ -3,7 +3,10 @@ export default {
     name: "info",
     description: "Get information about the Matrix AntiCheat.",
     requireOp: false,
-    execute: (args, player) => {
-        
+    execute: () => {
+        return {
+            status: 0,
+            message: "§7[§aMatrix§7]§7 §fMatrix Anticheat Version 7.0.0\nAuthor: jasonlaubb (Discord: uwu_the_great)\nGitHub: https://github.com/jasonlaubb/Matrix-AntiCheat\nCurseForge: https://www.curseforge.com/minecraft-bedrock/addons/matrix-anti\nThis project is §eopen source§f with §cAGPLv3§f license.\nPlease join our §bDiscord support server§f for more information: §ebit.ly/matrix-dc§f or §ehttps://discord.gg/CqZGXeRKPJ"
+        }
     }
 } as Command;
--- bp/src/main.ts
+++ bp/src/main.ts
@@ -1,4 +1,5 @@
 import { CustomCommandResult, CustomCommandParamType, Player, system } from "@minecraft/server";
+import info from "./command/info";
 // §7[§aMatrix§7]§7 §f
 Player.prototype.isOp = function() {
     return this.commandPermissionLevel >= 2;
@@ -16,10 +17,10 @@ export interface Command {
     optionalParameters?: Option[];
     parameters?: Option[];
     /** @warning Early execution, please add system.run if you want to do edit to world */
-    execute: (args: string[], player: Player) => CustomCommandResult;
+    execute: (player: Player, args: string[]) => CustomCommandResult;
 }
 system.beforeEvents.startup.subscribe((event) => {
-    const commands = [] as Command[];
+    const commands = [info] as Command[];
     function convertType(type: string): CustomCommandParamType {
         switch (type) {
             case "string":
@@ -99,7 +100,7 @@ system.beforeEvents.startup.subscribe((event) => {
                 }
             }
             try {
-                return execute(args, player);
+                return execute(player, args);
             } catch (error) {
                 return { status: 1, message: `§7[§aMatrix§7]§7 §fAn unexpected error occurred while executing the command. Please report this bug to the developer:§e\n${(error as Error).name}: ${(error as Error).message}\n${(error as Error).stack ?? "-- Stack is undefined --"}` };
             }
