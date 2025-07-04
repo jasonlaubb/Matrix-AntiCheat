9bf8a498d7517576db4d7ad9f4bd4d815f8f6094
jasonlaubb <jasonlau5378@gmail.com>
2025-07-04 12:27:41 +0800

    awa

---
+++ rollup.mjs
@@ -0,0 +1,35 @@
+import { rollup } from "rollup";
+import path from "path";
+import fs from "fs";
+import terser from '@rollup/plugin-terser';
+
+async function bundleJS() {
+    console.log("Start bundling");
+    const now = Date.now();
+    // Remove previous build artifacts
+    fs.unlink("dist/tsconfig.tsbuildinfo", (_error) => {});
+    const bundle = await rollup({
+        input: path.resolve("dist/main.js"),
+        external: ["@minecraft/server", "@minecraft/server-ui", "../data/property", "./data/property"],
+        plugins: [terser({
+            sourceMap: false,
+            compress: true,
+            format: {
+                comments: false,
+            }
+        })]
+    });
+
+    await bundle.write({
+        file: path.resolve("bp/scripts/main.js"),
+        format: "es",
+        sourcemap: false,
+        compact: true,
+        preserveModules: false,
+        strict: false,
+    });
+
+    console.log(`Bundling finished (${Date.now() - now}ms)`);
+}
+fs.copyFileSync("dist/data/property.js", "bp/scripts/data/property.js");
+bundleJS().catch(console.error);
