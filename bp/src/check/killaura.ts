92f99e7551777e0ca66955ba89e646ce36c38204
jasonlaubb <jasonlau5378@gmail.com>
2025-07-05 15:42:15 +0800

    Sorry

--- bp/src/check/killaura.ts
+++ bp/src/check/killaura.ts
@@ -79,9 +79,9 @@ function entityHurt({ hurtEntity, damageSource: { damagingEntity: attacker, dama
                 const newRec1 = attackerRecords.concat(attacker.antiReachRecords!);
                 const newRec2 = hurtEntityRecords.concat(hurtEntity.antiReachRecords!);
                 const reachDistance = lineDistance(newRec1, newRec2);
-                attacker.sendMessage(`Reach distance: ` + reachDistance + "/" + (absPitch < 50 && height >= 2 ? 4.55 : 4.25));
+                attacker.sendMessage(`Reach distance: ` + reachDistance + "/" + (absPitch < 50 && height >= 2 ? 4.51 : 4.01));
                 attacker.sendMessage(attacker.killauraFlag.toString());
-                if (reachDistance > (absPitch < 50 && height >= 2 ? 4.05 : 4.55)) {
+                if (reachDistance > (absPitch < 50 && height >= 2 ? 4.51 : 4.01)) {
                     attacker.sendMessage("Flagged!")
                     attacker.killauraFlag++;
                     attacker.killauraLastFlag = now;
