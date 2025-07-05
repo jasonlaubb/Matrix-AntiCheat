2ffded6bbae0b6c4e13e9e1a08ef1622482ca72b
jasonlaubb <jasonlau5378@gmail.com>
2025-07-05 17:43:56 +0800

    w

--- bp/src/check/nofall.ts
+++ bp/src/check/nofall.ts
@@ -1,5 +1,6 @@
-import { Player } from "@minecraft/server";
+import { EntityDamageCause, Player } from "@minecraft/server";
 import { addCheckInterval } from "../util/tick";
+import { max2 } from "../util/mathUtil";
 export default {
     enable() {
         addCheckInterval(tick);
@@ -7,7 +8,11 @@ export default {
 }
 function tick (player: Player) {
     if (!player.isFalling && player.nofallLastFallState) {
+        const fallDistance = player.nofallLastOnGroundLocation.y - player.location.y;
         player.sendMessage(`Fall for ` + (player.nofallLastOnGroundLocation.y - player.location.y) + ` block(s)`);
+        player.applyDamage(max2(0, fallDistance - 3), {
+            cause: EntityDamageCause.fall,
+        });
     }
     player.nofallLastFallState = player.isFalling;
     if (player.isOnGround) player.nofallLastOnGroundLocation = player.location;
