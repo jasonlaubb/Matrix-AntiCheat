14a5a8953d99755f0e08b68a51927ffbd33d5e90
jasonlaubb <jasonlau5378@gmail.com>
2025-07-05 20:09:42 +0800

    Pc cheater bad

--- bp/src/check/chestaura.ts
+++ bp/src/check/chestaura.ts
@@ -10,10 +10,10 @@ export default {
 }
 
 function interact (event: PlayerInteractWithBlockBeforeEvent) {
-    if (event.block.typeId !== "minecraft:chest" || distanceXZ(event.player.location, event.block.location)) return;
-    if (distanceXZ(event.player.location, event.block.location) < 2) return;
+    if (event.block.typeId !== "minecraft:chest" || distanceXZ(event.player.location, event.block.location) < 2) return;
     const angle = calculateRelativeViewAngle(event.player.location, event.block.center(), event.player.getRotation().y);
-    if (angle > (event.player.inputInfo.lastInputModeUsed === "Touch" ? 120 : 50)) {
+    system.run(() => event.player.sendMessage(angle.toString()));
+    if (angle > (event.player.inputInfo.lastInputModeUsed === "Touch" ? 120 : 12)) {
         event.cancel = true;
         system.run(() => event.player.flag("ChestAura", "A", "Player", { angle: angle.toFixed(2) }));
     }
