d451a61679833be2faca19c55305f08fbc70060c
jasonlaubb <jasonlau5378@gmail.com>
2025-07-05 20:12:58 +0800

    11 too low false pos wor

--- bp/src/check/chestaura.ts
+++ bp/src/check/chestaura.ts
@@ -13,7 +13,7 @@ function interact (event: PlayerInteractWithBlockBeforeEvent) {
     if (event.block.typeId !== "minecraft:chest" || distanceXZ(event.player.location, event.block.location) < 2) return;
     const angle = calculateRelativeViewAngle(event.player.location, event.block.center(), event.player.getRotation().y);
     system.run(() => event.player.sendMessage(angle.toString()));
-    if (angle > (event.player.inputInfo.lastInputModeUsed === "Touch" ? 120 : 12)) {
+    if (angle > (event.player.inputInfo.lastInputModeUsed === "Touch" ? 120 : 30)) {
         event.cancel = true;
         system.run(() => event.player.flag("ChestAura", "A", "Player", { angle: angle.toFixed(2) }));
     }
