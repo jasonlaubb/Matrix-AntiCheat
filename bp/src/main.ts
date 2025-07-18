75342b0d4c306633919c99624de8fc3cc23f0602
jasonlaubb <jasonlau5378@gmail.com>
2025-07-18 09:19:56 +0800

    awa

--- bp/src/main.ts
+++ bp/src/main.ts
@@ -126,7 +126,7 @@ system.beforeEvents.startup.subscribe((event) => {
                 }
                 for (let i = 0; i < args.length; i++) {
                     const input = args[i];
-                    const param = parameters![i] ?? optionalParameters![i - parameters!.length];
+                    const param = parameters![i] ?? optionalParameters![i - (parameters?.length ?? 0)];
                     if (param === undefined) continue;
                     switch (param.type) {
                         case "float":
@@ -178,6 +178,8 @@ system.beforeEvents.startup.subscribe((event) => {
                                     };
                                 }
                             }
+                            //@ts-ignore
+                            console.log(JSON.stringify(input));
                             args[i] = input[0];
                             break;
                         }
