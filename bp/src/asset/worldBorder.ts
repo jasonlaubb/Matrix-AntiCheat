46b6d5b3b74aa4e7fac0485da534ebab9ca8ef9e
jasonlaubb <jasonlau5378@gmail.com>
2025-07-25 17:26:55 +0800

    awa

--- bp/src/asset/worldBorder.ts
+++ bp/src/asset/worldBorder.ts
@@ -37,16 +37,16 @@ function tickEvent(player: Player) {
     }
     if (get("worldBorderEffect")) {
         if (fastAbs(size - xDiff) <= 7) {
-            const targetX = x1 + size;
-            const startZ = z1 - 5;
-            for (let i = 0; i < 10; i++) {
+            const targetX = x2 + size;
+            const startZ = z1 - 20;
+            for (let i = 0; i < 20; i++) {
                 player.dimension.spawnParticle(particleId, { x: targetX, y, z: startZ + i });
             }
         }
         if (fastAbs(size - zDiff) <= 7) {
-            const targetZ = z1 + size;
+            const targetZ = z2 + size;
             const startX = x1 - 5;
-            for (let x = 0; x < 10; x++) {
+            for (let x = 0; x < 20; x++) {
                 player.dimension.spawnParticle(particleId, { x: startX + x, y, z: targetZ });
             }
         }
