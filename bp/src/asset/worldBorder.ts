4923836e85b9c445a6964d5b5637dc2a2c8f5c71
jasonlaubb <jasonlau5378@gmail.com>
2025-07-25 18:48:30 +0800

    awa

--- bp/src/asset/worldBorder.ts
+++ bp/src/asset/worldBorder.ts
@@ -15,7 +15,7 @@ export function worldBorderOff() {
     world.beforeEvents.playerPlaceBlock.unsubscribe(blockChange);
     world.beforeEvents.playerInteractWithBlock.unsubscribe(blockChange);
 }
-const particleId = "minecraft:raid_omen_emitter";
+const particleId = "minecraft:note_particle";
 function tickEvent(player: Player) {
     const size = get("worldBorderSize") as number;
     const { x: x1, y, z: z1 } = player.location;
@@ -36,27 +36,45 @@ function tickEvent(player: Player) {
         player.lastDimension = player.dimension.id;
     }
     if (get("worldBorderEffect")) {
-        if (fastAbs(size - xDiff) <= 7) {
-            const targetX = x1 > x2 ? x2 + size : x2 - size;
-            const startZ = z1 - 10;
-            for (let i = 0; i < 20; i++) {
-                player.dimension.spawnParticle(particleId, { x: targetX, y, z: startZ + i });
-            }
+    const xDist = fastAbs(size - xDiff);
+    const zDist = fastAbs(size - zDiff);
+
+    const nearX = xDist <= 7;
+    const nearZ = zDist <= 7;
+
+    if (nearX && nearZ) {
+        const targetX = x1 > x2 ? x2 + size : x2 - size;
+        const targetZ = z1 > z2 ? z2 + size : z2 - size;
+
+        // L-shape: vertical line along Z
+        for (let dz = 0; dz < 20; dz++) {
+            player.dimension.spawnParticle(particleId, { x: targetX, y, z: targetZ - (z1 > z2 ? dz : -dz) });
         }
-        if (fastAbs(size - zDiff) <= 7) {
-            const targetZ = z1 > z2 ? z2 + size : z2 - size;
-            const startX = x1 - 10;
-            for (let x = 0; x < 20; x++) {
-                player.dimension.spawnParticle(particleId, { x: startX + x, y, z: targetZ });
-            }
+
+        // L-shape: horizontal line along X
+        for (let dx = 0; dx < 20; dx++) {
+            player.dimension.spawnParticle(particleId, { x: targetX - (x1 > x2 ? dx : -dx), y, z: targetZ });
+        }
+    } else if (nearX) {
+        const targetX = x1 > x2 ? x2 + size : x2 - size;
+        const startZ = z1 - 10;
+        for (let i = 0; i < 20; i++) {
+            player.dimension.spawnParticle(particleId, { x: targetX, y, z: startZ + i });
+        }
+    } else if (nearZ) {
+        const targetZ = z1 > z2 ? z2 + size : z2 - size;
+        const startX = x1 - 10;
+        for (let x = 0; x < 20; x++) {
+            player.dimension.spawnParticle(particleId, { x: startX + x, y, z: targetZ });
         }
     }
+}
 }
 function blockChange(event: PlayerBreakBlockBeforeEvent | PlayerPlaceBlockBeforeEvent | PlayerInteractWithBlockBeforeEvent) {
     const { x, z } = event.block.location;
     const { x: x2, z: z2 } = world.getDefaultSpawnLocation();
     const size = get("worldBorderSize") as number;
-    if (fastAbs(x - x2) > size || fastAbs(z - z2) > size) {
+    if (fastAbs(x - x2) >= size || fastAbs(z - z2) >= size) {
         event.cancel = true;
     }
 }
