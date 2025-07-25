50ee9e9c4386ac850dc73bdc4cbd91a4c19a4f79
jasonlaubb <jasonlau5378@gmail.com>
2025-07-25 18:27:19 +0800

    awa

--- bp/src/command/worldBorder.ts
+++ bp/src/command/worldBorder.ts
@@ -1,4 +1,4 @@
-import { system } from "@minecraft/server";
+import { system, world } from "@minecraft/server";
 import { worldBorderOff, worldBorderOn } from "../asset/worldBorder";
 import type { Command } from "../main";
 import { get } from "../util/database";
@@ -12,6 +12,7 @@ export default {
             if (isEnabled) {
                 worldBorderOff();
             } else worldBorderOn();
+            world.setDynamicProperty("database:worldBorder", !isEnabled);
         });
         return { status: 0, message: `§7[§aMatrix§7] §fSuccessfully ${isEnabled ? "disabled" : "enabled"} world border feature. To change the border size, you can use '/setnumber worldBorderSize <size>'` };
     },
