a073de3289d23c063017c7195c39a83e4a244e10
jasonlaubb <jasonlau5378@gmail.com>
2025-07-27 13:29:23 +0800

    awa

--- bp/src/asset/oreAlert.ts
+++ bp/src/asset/oreAlert.ts
@@ -28,7 +28,7 @@ function blockBreak (event: PlayerBreakBlockAfterEvent) {
         event.player.lastOreFoundData ??= {};
         world.getAllPlayers().forEach((player) => {
             if (!player.isOp()) return;
-            player.sendMessage(`§7[§aOre Alert§7] §e${event.player.name} has just broken §e${id.replace("minecraft:", "").replace("_", " ")} §b[interval=${event.player.lastDiamondOresFound ? Math.floor((now - event.player.lastDiamondOresFound) / 1000) + "s" : "none"}]`);
+            player.sendMessage(`§7[§aOre Alert§7] §e${event.player.name} has just broken §e${id.replace("minecraft:", "").replace("_", " ")} §b[interval=${event.player.lastOreFoundData[id] ? Math.floor((now - event.player.lastOreFoundData[id]) / 1000) + "s" : "none"}]`);
         });
         event.player.lastOreFoundData[id] = now;
     }
