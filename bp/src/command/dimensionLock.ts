bd87b97e28eb46cfa11379723b763ab6df3bce3c
jasonlaubb <jasonlau5378@gmail.com>
2025-07-28 13:03:28 +0800

    awa

--- bp/src/command/dimensionLock.ts
+++ bp/src/command/dimensionLock.ts
@@ -1,7 +1,7 @@
 import { system, world } from "@minecraft/server";
 import type { Command } from "../main";
 import { get } from "../util/database";
-import { endNetherLockOff, endNetherLockOn } from "../asset/endNetherLock";
+import { checkNetherEnd, endNetherLockOff, endNetherLockOn } from "../asset/endNetherLock";
 export const endLock = {
     name: "endlock",
     requireOp: true,
@@ -14,10 +14,10 @@ export const endLock = {
             if (isEnabled) {
                 endNetherLockOff();
             } else {
-                
+                checkNetherEnd();
                 endNetherLockOn();
             }
-            world.setDynamicProperty("database:worldBorder", !isEnabled);
+            world.setDynamicProperty("database:endLock", !isEnabled);
         });
         return { status: 0, message: `§7[§aMatrix§7] §fSuccessfully ${isEnabled ? "disabled" : "enabled"} end lock.` };
     },
@@ -33,8 +33,11 @@ export const netherLock = {
             if (isEnabled2) return;
             if (isEnabled) {
                 endNetherLockOff();
-            } else endNetherLockOn();
-            world.setDynamicProperty("database:worldBorder", !isEnabled);
+            } else {
+                checkNetherEnd();
+                endNetherLockOn();
+            }
+            world.setDynamicProperty("database:netherLock", !isEnabled);
         });
         return { status: 0, message: `§7[§aMatrix§7] §fSuccessfully ${isEnabled ? "disabled" : "enabled"} nether lock.` };
     },
