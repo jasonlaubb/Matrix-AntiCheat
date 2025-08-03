43a1394d65283aff26f82722bfff9bf5f7ec9c96
jasonlaubb <jasonlau5378@gmail.com>
2025-08-03 18:07:43 +0800

    awa

--- bp/src/check/autoclicker.ts
+++ bp/src/check/autoclicker.ts
@@ -22,7 +22,7 @@ function onEntityHit ({ damagingEntity: player }: EntityHitEntityAfterEvent) {
                     player.autoclickerFlag++;
                     player.autoclickerLastFlag = now;
                     if (get("antiAutoClickerWarning")) {
-                        player.sendMessage("§7[§aMatrix§7] §cAuto-clicker §fis not allowed and you §cmay be punished§f if you continue using such unfair advantage.");
+                        player.sendMessage("§c§lHey!§r§7 Auto clicker is not allowed!");
                         world.getAllPlayers().forEach((target) => {
                             if (!target.isOp()) return;
                             player.sendMessage(`§7[§aMatrix§7] §e${player.name} §fhas triggered auto-clicker flag.`);
