37ae2b874d33f1417d69b7e2a89b4701478c088f
jasonlaubb <jasonlau5378@gmail.com>
2025-08-04 18:22:36 +0800

    Almost forgot to impl Disabler/A

--- bp/src/check/fly.ts
+++ bp/src/check/fly.ts
@@ -125,7 +125,7 @@ function tick(player: Player) {
         const item = player.getComponent("equippable")!.getEquipment(EquipmentSlot.Chest);
         if (!item || item.typeId !== "minecraft:elytra") {
             data.lastFlagTimestamp = now;
-            player.flag("Fly", "C", "Movement (GlideTag)");
+            player.flag("Fly", "E", "Movement (GlideTag)");
         }
     }
     data.lastVelocityY = velocityY;
