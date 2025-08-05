d0f6a8b245a2be69a8d566aa543bce9a5fb54df1
jasonlaubb <jasonlau5378@gmail.com>
2025-08-05 16:44:43 +0800

    I think this is better

--- bp/src/check/scaffold.ts
+++ bp/src/check/scaffold.ts
@@ -1,5 +1,5 @@
-import { PlayerPlaceBlockBeforeEvent, world, Block, system, GameMode, LocationOutOfWorldBoundariesError } from "@minecraft/server";
-import { getSurround, locEqual } from "../util/util";
+import { PlayerPlaceBlockBeforeEvent, world, Block, system, GameMode, LocationOutOfWorldBoundariesError, Direction } from "@minecraft/server";
+import { locEqual } from "../util/util";
 import { calculateRelativeViewAngle, distanceXZ } from "../util/mathUtil";
 export default {
     property: "antiScaffoldEnable",
@@ -12,21 +12,12 @@ export default {
 };
 
 function blockPlace(event: PlayerPlaceBlockBeforeEvent) {
-    const { block, player } = event;
+    const { block, player, face } = event;
     const height = player.location.y - block.location.y;
     if (player.isOp() || player.isFlying || player.getGameMode() === GameMode.Creative || height < 1) return;
-    const surroundBlock = getSurround(block)?.filter((block) => block && !block.isAir && !block.isLiquid);
-    // Only 1 surround block
-    if (surroundBlock) {
-        if (surroundBlock.length === 1) {
-            const supportBlock = surroundBlock[0]!;
-            if (supportBlock.location.y > block.location.y && supportBlock.location.y - player.location.y > 1) {
-                system.run(() => player.flag("Scaffold", "G", "Block (Downward)"));
-            }
-        } else if (surroundBlock.length === 0) {
-            event.cancel = true;
-            system.run(() => player.flag("Scaffold", "H", "Block (Invalid)"));
-        }
+    if (face === Direction.Down && event.faceLocation.y - player.location.y > 1) {
+        event.cancel = true;
+        system.run(() => player.flag("Scaffold", "G", "Player"));
     }
     if (height >= 2) return;
     const { x: pitch, y: yaw } = player.getRotation();
