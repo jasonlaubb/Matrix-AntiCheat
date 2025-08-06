b32b78d2682e75f1c3477dec97a59072c89618fe
jasonlaubb <jasonlau5378@gmail.com>
2025-08-06 11:26:17 +0800

    awa

--- bp/src/check/shulkerBoxNesting.ts
+++ bp/src/check/shulkerBoxNesting.ts
@@ -1,24 +1,62 @@
-import { BlockVolumeBase, PlayerBreakBlockAfterEvent, PlayerBreakBlockBeforeEvent, PlayerInteractWithBlockAfterEvent, PlayerPlaceBlockAfterEvent, system, world } from "@minecraft/server";
+import { Block, PlayerBreakBlockBeforeEvent, PlayerInteractWithBlockBeforeEvent, PlayerPlaceBlockAfterEvent, PlayerPlaceBlockBeforeEvent, system, world } from "@minecraft/server";
+export default {
+    property: "antiShulkerBoxNesting",
+    enable: () => {
+        world.beforeEvents.playerBreakBlock.subscribe(blockBreak);
+        world.beforeEvents.playerInteractWithBlock.subscribe(blockInteract);
+        world.afterEvents.playerPlaceBlock.subscribe(blockPlace);
+    },
+    disable: () => {
+        world.beforeEvents.playerBreakBlock.unsubscribe(blockBreak);
+        world.beforeEvents.playerInteractWithBlock.unsubscribe(blockInteract);
+        world.afterEvents.playerPlaceBlock.unsubscribe(blockPlace);
+    },
+}
 function blockBreak (event: PlayerBreakBlockBeforeEvent) {
     const block = event.block;
     if (block.typeId.startsWith("minecraft:") && block.typeId.endsWith("_shulker_box")) {
-        const blockContainer = block.getComponent("inventory")?.container;
-        if (!blockContainer) return;
-        let shulkerBoxIndex: number[] = [];
-        for (let i = 0; i < blockContainer.size; i++) {
-            const item = blockContainer.getItem(i);
-            if (item && item.typeId.startsWith("minecraft:") && item.typeId.endsWith("_shulker_box")) {
-                shulkerBoxIndex.push(i);
-            }
-        }
-        if (shulkerBoxIndex.length > 0) {
+        const data = getShulkerBoxIndex(block);
+        if (data && data.index.length > 0) {
             event.cancel = true;
             system.run(() => {
-                shulkerBoxIndex.forEach(index => blockContainer.setItem(index)); // Remove the shulker box item
+                data.index.forEach(index => data.blockContainer.setItem(index)); // Remove the shulker box item
                 const itemStack = block.getItemStack(1, true);
                 block.setType("minecraft:air");
-                if (itemStack) block.dimension.spawnItem(itemStack, block.center());
+                if (itemStack) block.dimension.spawnItem(itemStack, block.center()); // Spawn the item
             })
         }
     }
+}
+function blockInteract (event: PlayerInteractWithBlockBeforeEvent) {
+    const block = event.block;
+    if (block.typeId.startsWith("minecraft:") && block.typeId.endsWith("_shulker_box")) {
+        const data = getShulkerBoxIndex(block);
+        if (data && data.index.length > 0) {
+            event.cancel = true;
+            system.run(() => {
+                data.index.forEach(index => data.blockContainer.setItem(index)); // Remove the shulker box item
+            });
+        }
+    }
+}
+function blockPlace (event: PlayerPlaceBlockAfterEvent) {
+    const block = event.block;
+    if (block.typeId.startsWith("minecraft:") && block.typeId.endsWith("_shulker_box")) {
+        const data = getShulkerBoxIndex(block);
+        if (data && data.index.length > 0) {
+            data.index.forEach(index => data.blockContainer.setItem(index)); // Remove the shulker box item
+        }
+    }
+}
+function getShulkerBoxIndex (block: Block) {
+    const blockContainer = block.getComponent("inventory")?.container;
+    if (!blockContainer) return;
+    let shulkerBoxIndex: number[] = [];
+    for (let i = 0; i < blockContainer.size; i++) {
+        const item = blockContainer.getItem(i);
+        if (item && item.typeId.startsWith("minecraft:") && item.typeId.endsWith("_shulker_box")) {
+            shulkerBoxIndex.push(i);
+        }
+    }
+    return { index: shulkerBoxIndex, blockContainer };
 }
