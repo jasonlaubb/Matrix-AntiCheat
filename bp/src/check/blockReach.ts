2eeaf3cee65759be2cf6316d9748168b60a44eed
jasonlaubb <136591629+jasonlaubb@users.noreply.github.com>
2025-08-15 15:32:59 +0800

    Update blockReach.ts

--- bp/src/check/blockReach.ts
+++ bp/src/check/blockReach.ts
@@ -18,7 +18,7 @@ function blockEvent (event: PlayerInteractWithBlockBeforeEvent | PlayerBreakBloc
     if (player.isOp() || player.getGameMode() === GameMode.Creative) return;
     const pos = block.center();
     const reach = distance(pos, player.getHeadLocation());
-    const reachLimit = player.isFalling && player.location.y < block.location.y ? 5 : 6;
+    const reachLimit = player.isFalling && player.location.y < block.location.y ? 6 : 7;
     if (reach > reachLimit) {
         event.cancel = true;
         system.run(() => player.flag("BlockReach", "A", "World", { reach }));
