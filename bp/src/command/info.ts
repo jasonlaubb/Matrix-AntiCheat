b4c2b95c6808faad2fa3f805492f8ed8071b405c
jasonlaubb <jasonlau5378@gmail.com>
2025-08-21 11:19:54 +0800

    awa

--- bp/src/command/info.ts
+++ bp/src/command/info.ts
@@ -7,7 +7,7 @@ export default {
         return {
             status: 0,
             message:
-                "§7[§aMatrix§7]§7 §fMatrix Anticheat Version 7.0.16\nAuthor: jasonlaubb (Discord: uwu_the_great)\nGitHub: https://github.com/jasonlaubb/Matrix-AntiCheat\nCurseForge: https://www.curseforge.com/minecraft-bedrock/addons/matrix-anti\nThis project is §eopen source§f with §cAGPLv3§f license.\nPlease join our §bDiscord support server§f for more information: §ebit.ly/matrix-dc§f or §ehttps://discord.gg/CqZGXeRKPJ",
+                "§7[§aMatrix§7]§7 §fMatrix Anticheat Version 7.0.17\nAuthor: jasonlaubb (Discord: uwu_the_great)\nGitHub: https://github.com/jasonlaubb/Matrix-AntiCheat\nCurseForge: https://www.curseforge.com/minecraft-bedrock/addons/matrix-anti\nThis project is §eopen source§f with §cAGPLv3§f license.\nPlease join our §bDiscord support server§f for more information: §ebit.ly/matrix-dc§f or §ehttps://discord.gg/CqZGXeRKPJ",
         };
     },
 } as Command;
