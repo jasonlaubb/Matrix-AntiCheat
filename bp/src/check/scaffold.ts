46e6da9ac863512aed355e01bd3eb76be771c559
jasonlaubb <jasonlau5378@gmail.com>
2025-08-27 11:02:53 +0800

    awa

--- bp/src/check/scaffold.ts
+++ bp/src/check/scaffold.ts
@@ -34,7 +34,7 @@ function blockPlace(event: PlayerPlaceBlockBeforeEvent) {
     }
     player.scaffoldNoRotationFlag ??= 0;
     const onlyTouchedBlock = isScaffold && getOnlyTouchBlock(event.block);
-    if (isScaffold && pitch < (isTouchInput ? 45 : 37) && (distance <= 2.5 || (onlyTouchedBlock && distanceXZ(center(onlyTouchedBlock), event.player.location) <= distance))) {
+    if (isScaffold && pitch < (isTouchInput ? 45 : 40) && (distance <= 2.5 || (onlyTouchedBlock && distanceXZ(center(onlyTouchedBlock), event.player.location) <= distance))) {
         player.scaffoldNoRotationFlag++;
         if (player.scaffoldNoRotationFlag >= 6) {
             event.cancel = true;
