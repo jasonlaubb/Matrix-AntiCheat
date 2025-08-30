d93cf129736d2821049344ea2d229d90c0d36f92
jasonlaubb <jasonlau5378@gmail.com>
2025-08-30 21:15:59 +0800

    awa

--- bp/src/check/scaffold.ts
+++ bp/src/check/scaffold.ts
@@ -1,4 +1,4 @@
-import { Block, Direction, InputMode, PlayerPlaceBlockBeforeEvent, system, Vector3, VectorXZ, world } from "@minecraft/server";
+import { Block, Direction, GameMode, InputMode, PlayerPlaceBlockBeforeEvent, system, Vector3, VectorXZ, world } from "@minecraft/server";
 import { fastAbs } from "../util/mathUtil";
 export default {
     property: "antiScaffoldEnable",
@@ -11,6 +11,8 @@ export default {
 };
 function onblockPlace(event: PlayerPlaceBlockBeforeEvent) {
     const { player, face, faceLocation, block } = event;
+    const gamemode = player.getGameMode();
+    if ([GameMode.Creative, GameMode.Spectator].includes(gamemode) || player.isFlying) return;
     const height = player.location.y - block.location.y;
     const { x: pitch, y: yaw } = player.getRotation();
     const data: typeof player.scaffoldData = player.scaffoldData ?? {};
