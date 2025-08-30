c2180d813697abcc695f4a4412f501f33f2f006b
jasonlaubb <jasonlau5378@gmail.com>
2025-08-30 19:21:43 +0800

    awa

--- bp/src/check/scaffold.ts
+++ bp/src/check/scaffold.ts
@@ -1,4 +1,4 @@
-import { PlayerPlaceBlockBeforeEvent, system, Vector3, world } from "@minecraft/server";
+import { Block, Direction, InputMode, PlayerPlaceBlockBeforeEvent, system, Vector3, VectorXZ, world } from "@minecraft/server";
 import { fastAbs } from "../util/mathUtil";
 export default {
     property: "antiScaffoldEnable",
@@ -25,25 +25,52 @@ function onblockPlace (event: PlayerPlaceBlockBeforeEvent) {
         data.turnAmount = 0;
     }
     player.sendMessage(String(data.turnAmount / data.quickPlaceAmount) + " | " + pitch + "|" + interval + " | " + isSafeBridge(faceLocation));
+    const safeBridge = isSafeBridge(faceLocation); // A method to bridge with only hold instead of fast click
     if (data.quickPlaceAmount > 8) {
         const steeringRate = data.turnAmount / data.quickPlaceAmount;
-        if (steeringRate > 0.4 || steeringRate > 0.25 && isSafeBridge(faceLocation)) {
-            system.run(() => player.flag("Scaffold", "A", "Block", { steeringRate }));
+        if (steeringRate > 0.4 || steeringRate > 0.25 && safeBridge) {
+            system.run(() => player.flag("Scaffold", "A", "Block"));
         }
     }
-    if (!isSafeBridge(faceLocation)) {
+    const input = player.inputInfo;
+    const hasCrosshair = input.lastInputModeUsed !== InputMode.Touch || input.touchOnlyAffectsHotbar;
+    if (safeBridge) {
         data.startSafeBridgeDirection = face;
         data.startSafeBridgePitch = pitch;
-    } else {
-        if (data.startSafeBridgeDirection !== face || fastAbs(data.startSafeBridgePitch - pitch) > 20) {
+        if (hasCrosshair && pitch < 17) {
             system.run(() => player.flag("Scaffold", "B", "Block", { deltaPitch: fastAbs(data.startSafeBridgePitch - pitch) }));
         }
+    } else if (data.startSafeBridgeDirection !== face || fastAbs(data.startSafeBridgePitch - pitch) > 20) {
+        system.run(() => player.flag("Scaffold", "C", "Block", { deltaPitch: fastAbs(data.startSafeBridgePitch - pitch) }));
     }
-    data.lastPlaceDirection = face;
+    if (hasCrosshair && pitch < 49 && !safeBridge && isForwardScaffold(getBlockFaceXZ(block, face), player.location, face)) {
+        system.run(() => player.flag("Scaffold", "D", "Block", { pitch }));
+    }
     data.lastPlace = now;
     data.lastPitch = pitch;
     player.scaffoldData = data;
 }
 function isSafeBridge ({ x, y, z }: Vector3) {
     return x === 0 && y === 0 && z === 0;
+}
+function isForwardScaffold(facePos: VectorXZ, playerPos: Vector3, faceDirection: Direction) {
+    const dx = playerPos.x - facePos.x;
+    const dz = playerPos.z - facePos.z;
+    switch (faceDirection) {
+        case Direction.North: return dz > 0;
+        case Direction.South: return dz < 0;
+        case Direction.East:  return dx < 0;
+        case Direction.West:  return dx > 0;
+        default: return false;
+    }
+}
+function getBlockFaceXZ (block: Block, blockFace: Direction): VectorXZ {
+    const { x, z } = block.center()
+    switch (blockFace) {
+        case Direction.North: return { x, z: z - 0.5 };
+        case Direction.South: return { x, z: z + 0.5 };
+        case Direction.East: return { x: x + 0.5, z };
+        case Direction.West: return { x: x - 0.5, z };
+    }
+    return { x, z };
 }
