8df0ffd93f27134f68b8dfb7cac46c1f7420354b
jasonlaubb <jasonlau5378@gmail.com>
2025-08-30 16:45:08 +0800

    awa

--- bp/src/check/invalidSprint.ts
+++ bp/src/check/invalidSprint.ts
@@ -18,7 +18,7 @@ function tickEvent(player: Player) {
     if (player.getEffect("minecraft:blindness") && player.invalidSprintBlindAt && now - player.invalidSprintBlindAt > 500) {
         player.flag("InvalidSprint", "A", "Movement", { blindDuration: now - player.invalidSprintBlindAt });
     }
-    if (player.isSneaking) player.flag("InvalidSprint", "B", "Movement");
+    if (player.isSneaking && noSwiftSneak(player)) player.flag("InvalidSprint", "B", "Movement");
     const hunger = player.getComponent("player.hunger");
     // According to wiki, player cannot sprint when hunger value reached 6
     if (hunger && hunger.currentValue <= 6) {
@@ -43,6 +43,9 @@ function tickEvent(player: Player) {
             }
         }
     }
+}
+function noSwiftSneak (player: Player) {
+    return !player.getComponent("equippable")?.getEquipment(EquipmentSlot.Legs)?.getComponent("enchantable")?.hasEnchantment("swift_sneak");
 }
 function effectAdd({ entity, effectType }: EffectAddBeforeEvent) {
     if (effectType !== "minecraft:blindness" || !(entity instanceof Player) || entity.getEffect("minecraft:blindness")) return;
