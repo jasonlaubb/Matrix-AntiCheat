887df22d51bee0fe1ffea94f145241032e444081
jasonlaubb <jasonlau5378@gmail.com>
2025-08-31 10:56:22 +0800

    awa

--- bp/src/check/autototem.ts
+++ bp/src/check/autototem.ts
@@ -19,13 +19,13 @@ function tickEvent(player: Player) {
             if (player.autototemLastItem[1]) {
                 offhand.setItem();
                 player.getComponent("inventory")!.container!.addItem(hasItem);
-                player.flag("AutoTotem", "A", "Player", { item: hasItem.typeId });
+                player.flag("AutoTotem", "A", "Player", { item: hasItem.typeId }); // Wear totem in a tick
             } else {
                 const { x, y } = player.inputInfo.getMovementVector();
                 if (x !== 0 || y !== 0) {
                     offhand.setItem();
                     player.getComponent("inventory")!.container!.addItem(hasItem);
-                    player.flag("AutoTotem", "B", "Player", { item: hasItem.typeId });
+                    player.flag("AutoTotem", "B", "Player", { item: hasItem.typeId }); // Wear totem when they are moving
                 }
             }
         }
