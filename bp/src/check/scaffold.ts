75e4ff99afaf02d6c75ec207a77ded09ca2abdaa
jasonlaubb <jasonlau5378@gmail.com>
2025-08-31 11:15:15 +0800

    qwq

--- bp/src/check/scaffold.ts
+++ bp/src/check/scaffold.ts
@@ -58,7 +58,7 @@ function onblockPlace(event: PlayerPlaceBlockBeforeEvent) {
         const pitchDelta = fastAbs(data.startSafeBridgePitch - pitch);
         // Jump bridge is looking down with low extender, we can ignore them
         if (pitchDelta > 15 && !(extender < 1.7 && pitch > 80) || !hasCrosshair || data.startSafeBridgeDirection !== face) {
-            system.run(() => player.flag("Scaffold", "C", "Block", { deltaPitch: fastAbs(data.startSafeBridgePitch - pitch) }));
+            system.run(() => player.flag("Scaffold", "C", "Block", { pitchDelta, lastDir: data.startSafeBridgeDirection, face }));
         }
     }
     const isClickScaffold = !safeBridge && forwardScaffold; // Check if the scaffold is a forward bridge that by 1-click (not by hold)
