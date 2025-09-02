2e3e86fdba6d155eb52dc5c8ca698303b43d72b0
jasonlaubb <jasonlau5378@gmail.com>
2025-09-02 16:41:27 +0800

    awa

--- bp/src/command/info.ts
+++ bp/src/command/info.ts
@@ -3,10 +3,7 @@ export default {
     name: "info",
     description: "Get information about the Matrix AntiCheat.",
     requireOp: false,
-    execute: (player) => {
-        player.sendMessage(
-            "§7[§aMatrix§7]§7 §fMatrix Anticheat Version 7.0.23\nAuthor: jasonlaubb (Discord: uwu_the_great)\nGitHub: https://github.com/jasonlaubb/Matrix-AntiCheat\nCurseForge: https://www.curseforge.com/minecraft-bedrock/addons/matrix-anti\nThis project is §eopen source§f with §cAGPLv3§f license.\nPlease join our §bDiscord support server§f for more information: §ebit.ly/matrix-dc§f or §ehttps://discord.gg/CqZGXeRKPJ"
-        );
-        return { status: 0 };
+    execute: () => {
+        return { status: 0, message: "§7[§aMatrix§7]§7 §fMatrix Anticheat Version 7.0.24\nAuthor: jasonlaubb (Discord: uwu_the_great)\nGitHub: https://github.com/jasonlaubb/Matrix-AntiCheat\nCurseForge: https://www.curseforge.com/minecraft-bedrock/addons/matrix-anti\nThis project is §eopen source§f with §cAGPLv3§f license.\nPlease join our §bDiscord support server§f for more information: §ebit.ly/matrix-dc§f or §ehttps://discord.gg/CqZGXeRKPJ" };
     },
 } as Command;
