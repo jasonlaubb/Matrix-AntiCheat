41a2f38f404e69114c4c6145391d4d25b253de77
jasonlaubb <136591629+jasonlaubb@users.noreply.github.com>
2025-09-10 16:22:01 +0800

    Update text.ts

--- bp/src/util/text.ts
+++ bp/src/util/text.ts
@@ -6,6 +6,17 @@ const languageList: { [key: string]: typeof english } = {
 function updateLanguage () {
       currentLanguage = languageList[get("systemLanguage")] ?? languageList.english;
 }
-function text () {
-      return currentLanguage;
+function text (key: keyof typeof english, ...args: string[]): string {
+      let string = currentLanguage[key];
+      const length = args.length;
+      if (length === 0) return;
+      for (let i = 0, i < length; i++) {
+          const arg = args[i];
+          if (i === 0) {
+              string = string.replace(/%s|%1/g, arg);
+              continue;
+          }
+          string = string.replaceAll("%" + i.toFixed(0));
+      }
+      return string;
 }
