ad7813ba5a658c2bbce39fa4623c089fd0cc226b
jasonlaubb <jasonlau5378@gmail.com>
2025-09-11 17:05:23 +0800

    awa

--- bp/src/util/text.ts
+++ bp/src/util/text.ts
@@ -1,14 +1,14 @@
 import english from "../data/languages/english";
 import { get } from "./database";
-export type TranslationKey = typeof english;
+export type TranslationKey = keyof typeof english;
 let currentLanguage = english;
-export const languageList: { [key: string]: TranslationKey } = {
+export const languageList: { [key: string]: typeof english } = {
     english,
 };
 export function updateLanguage () {
       currentLanguage = languageList[get("systemLanguage")] ?? languageList.english;
 }
-export function text(key: keyof typeof english, ...args: string[]): string {
+export function text(key: TranslationKey, ...args: string[]): string {
     let string = currentLanguage[key];
     if (args.length === 0) return string;
     args.forEach((arg, i) => {
