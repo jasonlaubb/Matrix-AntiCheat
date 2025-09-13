27df3f4e1c63a2b95c86f94dbecf3d855fca1eba
jasonlaubb <jasonlau5378@gmail.com>
2025-09-13 08:31:59 +0800

    awa

--- bp/src/command/gm.ts
+++ bp/src/command/gm.ts
@@ -1,38 +1,59 @@
 import { GameMode, system } from "@minecraft/server";
 import type { Command } from "../main";
+import english from "../data/languages/english";
+import { text } from "../util/text";
 export const gma = {
     requireOp: true,
     name: "gma",
-    description: "Switch your gamemode to advanture mode",
+    description: english.commandGmaDescription,
+    translationDef: {
+        actionName: "commandGma",
+        description: "commandGmaDescription"
+    },
     execute: (player) => {
         system.run(() => player.setGameMode(GameMode.Adventure));
-        return { status: 0, message: "§7[§aMatrix§7] §fSwitched your gamemode to advanture." };
+        return { status: 0, message: "§7[§aMatrix§7] §f" + text("commandGmaSuccess") };
     },
 } as Command;
+
 export const gmc = {
     requireOp: true,
     name: "gmc",
-    description: "Switch your gamemode to creative mode",
+    description: english.commandGmcDescription,
+    translationDef: {
+        actionName: "commandGmc",
+        description: "commandGmcDescription"
+    },
     execute: (player) => {
         system.run(() => player.setGameMode(GameMode.Creative));
-        return { status: 0, message: "§7[§aMatrix§7] §fSwitched your gamemode to creative." };
+        return { status: 0, message: "§7[§aMatrix§7] §f" + text("commandGmcSuccess") };
     },
 } as Command;
+
 export const gms = {
     requireOp: true,
     name: "gms",
-    description: "Switch your gamemode to survival mode",
+    description: english.commandGmsDescription,
+    translationDef: {
+        actionName: "commandGms",
+        description: "commandGmsDescription"
+    },
     execute: (player) => {
         system.run(() => player.setGameMode(GameMode.Survival));
-        return { status: 0, message: "§7[§aMatrix§7] §fSwitched your gamemode to survival." };
+        return { status: 0, message: "§7[§aMatrix§7] §f" + text("commandGmsSuccess") };
     },
 } as Command;
+
 export const gmsp = {
     requireOp: true,
     name: "gmsp",
-    description: "Switch your gamemode to spectator mode",
+    description: english.commandGmspDescription,
+    translationDef: {
+        actionName: "commandGmsp",
+        description: "commandGmspDescription"
+    },
     execute: (player) => {
         system.run(() => player.setGameMode(GameMode.Spectator));
-        return { status: 0, message: "§7[§aMatrix§7] §fSwitched your gamemode to spectator." };
+        return { status: 0, message: "§7[§aMatrix§7] §f" + text("commandGmspSuccess") };
     },
 } as Command;
