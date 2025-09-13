12e2d2ccafb235b74427d4e15c9c7065fc82e1c4
jasonlaubb <jasonlau5378@gmail.com>
2025-09-13 08:50:30 +0800

    awa

--- bp/src/command/set.ts
+++ bp/src/command/set.ts
@@ -3,142 +3,165 @@ import { world, system } from "@minecraft/server";
 import { getPropertyType } from "../util/propertyClassifier";
 import property from "../data/property";
 import { MessageFormData } from "@minecraft/server-ui";
+import english from "../data/languages/english";
+import { text } from "../util/text";
 export const setBoolean = {
     name: "setboolean",
-    description: "Change a boolean value (true/false) of config",
+    description: english.commandSetBooleanDescription,
     requireOp: true,
+    translationDef: {
+        actionName: "commandSetBoolean",
+        description: "commandSetBooleanDescription",
+        param: ["commandSetBooleanProperty", "commandSetBooleanValue"]
+    },
     parameters: [
-        {
-            name: "booleanProperty",
-            type: "enum",
-        },
-        {
-            name: "value",
-            type: "boolean",
-        },
+        { name: "booleanProperty", type: "enum" },
+        { name: "value", type: "boolean" },
     ],
     execute: (_player, [id, value]) => {
-        if (!getPropertyType().booleanValue.includes(id)) return { status: 1, message: "§7[§aMatrix§7] §fInvalid property id..." };
+        if (!getPropertyType().booleanValue.includes(id)) {
+            return { status: 1, message: "§7[§aMatrix§7] §f" + text("commandInvalidProperty") };
+        }
         system.run(() => world.setDynamicProperty("database:" + id, value));
         return {
             status: 0,
-            message: `§7[§aMatrix§7] §fSuccessfully changed property §e${id}§f to §e${value ? "true" : "false"}`,
+            message: "§7[§aMatrix§7] §f" + text("commandSetBooleanSuccess", id, value ? "true" : "false")
         };
     },
 } as Command;
+
 export const setString = {
     name: "setstring",
-    description: "Change a string value of config",
+    description: english.commandSetStringDescription,
     requireOp: true,
+    translationDef: {
+        actionName: "commandSetString",
+        description: "commandSetStringDescription",
+        param: ["commandSetStringProperty", "commandSetStringValue"]
+    },
     parameters: [
-        {
-            name: "stringProperty",
-            type: "enum",
-        },
-        {
-            name: "value",
-            type: "string",
-        },
+        { name: "stringProperty", type: "enum" },
+        { name: "value", type: "string" },
     ],
     execute: (_player, [id, value]) => {
-        if (!getPropertyType().stringValue.includes(id)) return { status: 1, message: "§7[§aMatrix§7] §fInvalid property id..." };
+        if (!getPropertyType().stringValue.includes(id)) {
+            return { status: 1, message: "§7[§aMatrix§7] §f" + text("commandInvalidProperty") };
+        }
         system.run(() => world.setDynamicProperty("database:" + id, value));
         return {
             status: 0,
-            message: `§7[§aMatrix§7] §fSuccessfully changed property §e${id}§f to §e${value}`,
+            message: "§7[§aMatrix§7] §f" + text("commandSetStringSuccess", id, value)
         };
     },
 } as Command;
+
 export const setNumber = {
     name: "setnumber",
-    description: "Change a string value of config",
+    description: english.commandSetNumberDescription,
     requireOp: true,
+    translationDef: {
+        actionName: "commandSetNumber",
+        description: "commandSetNumberDescription",
+        param: ["commandSetNumberProperty", "commandSetNumberValue"]
+    },
     parameters: [
-        {
-            name: "numberProperty",
-            type: "enum",
-        },
-        {
-            name: "value",
-            type: "float",
-        },
+        { name: "numberProperty", type: "enum" },
+        { name: "value", type: "float" },
     ],
     execute: (_player, [id, value]) => {
-        if (!getPropertyType().numberValue.includes(id)) return { status: 1, message: "§7[§aMatrix§7] §fInvalid property id..." };
+        if (!getPropertyType().numberValue.includes(id)) {
+            return { status: 1, message: "§7[§aMatrix§7] §f" + text("commandInvalidProperty") };
+        }
         system.run(() => world.setDynamicProperty("database:" + id, value));
         return {
             status: 0,
-            message: `§7[§aMatrix§7] §fSuccessfully changed property §e${id}§f to §e${value}`,
+            message: "§7[§aMatrix§7] §f" + text("commandSetNumberSuccess", id, value)
         };
     },
 } as Command;
+
 export const resetConfig = {
     name: "resetconfig",
-    description: "Reset all the changed properties (config) saved in dynamic properties",
+    description: english.commandResetConfigDescription,
     requireOp: true,
+    translationDef: {
+        actionName: "commandResetConfig",
+        description: "commandResetConfigDescription"
+    },
     execute: (player) => {
         const matches = world.getDynamicPropertyIds().filter((id) => id.startsWith("database:"));
-        if (matches.length === 0)
+        if (matches.length === 0) {
             return {
                 status: 1,
-                message: "§7[§aMatrix§7] §fYou have never changed any property...",
+                message: "§7[§aMatrix§7] §f" + text("commandResetConfigEmpty")
             };
+        }
         system.run(() => {
             new MessageFormData()
-                .title("Are you sure?")
-                .body("Are you sure to reset all the changed property in the config? This cannot be recovered.")
-                .button1("§4§lYes")
-                .button2("§2§lNo")
+                .title(text("commandResetConfigConfirmTitle"))
+                .body(text("commandResetConfigConfirmBody"))
+                .button1(text("commandResetConfigConfirmYes"))
+                .button2(text("commandResetConfigConfirmNo"))
                 //@ts-expect-error
                 .show(player)
                 .then((res) => {
                     if (res.canceled || res.selection === 1) return;
                     matches.forEach((id) => world.setDynamicProperty(id));
-                    player.sendMessage("§7[§aMatrix§7] §fSucessfully reset the config.");
+                    player.sendMessage("§7[§aMatrix§7] §f" + text("commandResetConfigSuccess"));
                 });
         });
         return { status: 0 };
     },
 } as Command;
+
 export const clearProperty = {
     name: "discard",
-    description: "Reset 1 of the property (config) saved in dyanamic properties",
+    description: english.commandClearPropertyDescription,
     requireOp: true,
+    translationDef: {
+        actionName: "commandClearProperty",
+        description: "commandClearPropertyDescription",
+        param: ["commandClearPropertyTarget"]
+    },
     parameters: [
-        {
-            name: "property",
-            type: "enum",
-        },
+        { name: "property", type: "enum" },
     ],
     execute: (_player, [id]) => {
-        if (!world.getDynamicProperty("database:" + id))
+        if (!world.getDynamicProperty("database:" + id)) {
             return {
                 status: 1,
-                message: "§7[§aMatrix§7] §fTarget property has not been changed.",
+                message: "§7[§aMatrix§7] §f" + text("commandClearPropertyNotChanged")
             };
+        }
         system.run(() => world.setDynamicProperty("database:" + id));
         return {
             status: 0,
-            message: "§7[§aMatrix§7] §fSucessfully reset the target property.",
+            message: "§7[§aMatrix§7] §f" + text("commandClearPropertySuccess")
         };
     },
 } as Command;
+
 export const getProperty = {
     name: "getproperty",
-    description: "View 1 of the property (config) saved in data and dynamic properties",
+    description: english.commandGetPropertyDescription,
     requireOp: true,
+    translationDef: {
+        actionName: "commandGetProperty",
+        description: "commandGetPropertyDescription",
+        param: ["commandGetPropertyTarget"]
+    },
     parameters: [
-        {
-            name: "property",
-            type: "enum",
-        },
+        { name: "property", type: "enum" },
     ],
     execute: (_player, [id]) => {
-        if (!Object.keys(property).includes(id)) return { status: 1, message: "§7[§aMatrix§7] §fInvalid property id..." };
+        if (!Object.keys(property).includes(id)) {
+            return { status: 1, message: "§7[§aMatrix§7] §f" + text("commandInvalidProperty") };
+        }
         const { type, value } = property[id as keyof typeof property];
+        const dynamic = world.getDynamicProperty("database:" + id) ?? "--";
         return {
             status: 0,
-            message: `§7[§aMatrix§7] §fThe value of property ${id}:\nType: §e${type}§f\nStatic data: §e${value}§r§f\nDynamic property: §e${world.getDynamicProperty("database:" + id) ?? "--"}`,
+            message: "§7[§aMatrix§7] §f" + text("commandGetPropertySuccess", id, type, "" + value, "" + dynamic)
         };
     },
 } as Command;
