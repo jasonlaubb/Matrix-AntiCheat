6ac77a88cf0c588b60334d4c714cb0d5c87dc51a
jasonlaubb <jasonlau5378@gmail.com>
2025-09-13 08:43:20 +0800

    awa

--- bp/src/command/itemui.ts
+++ bp/src/command/itemui.ts
@@ -1,11 +1,20 @@
 import { ItemStack, system } from "@minecraft/server";
 import type { Command } from "../main";
+import { text } from "../util/text";
+import english from "../data/languages/english";
 export default {
     name: "itemui",
-    description: "Get the admin gui item",
+    description: english.commandItemUiDescription,
     requireOp: true,
+    translationDef: {
+        actionName: "commandItemUi",
+        description: "commandItemUiDescription"
+    },
     execute: (player) => {
         system.run(() => player.getComponent("inventory")!.container.addItem(new ItemStack("matrix:ui_tool")));
-        return { status: 0, message: "§7[§aMatrix§7] §fYou have been given the ui tool. Holding the item and right-click (long-press on empty space for Pocket Edition) to open the gui." };
+        return {
+            status: 0,
+            message: "§7[§aMatrix§7] §f" + text("commandItemUiSuccess")
+        };
     },
 } as Command;
--- bp/src/command/language.ts
+++ bp/src/command/language.ts
@@ -1,20 +1,33 @@
 import { world } from "@minecraft/server";
 import type { Command } from "../main";
-import { languageList, updateLanguage } from "../util/text";
+import { languageList, text, updateLanguage } from "../util/text";
+import english from "../data/languages/english";
 export default {
     name: "language",
-    description: "Switch the system language of matrix anticheat",
+    description: english.commandLanguageDescription,
     requireOp: true,
+    translationDef: {
+        actionName: "commandLanguage",
+        description: "commandLanguageDescription",
+        param: ["commandLanguageValue"]
+    },
     parameters: [
-        {
-            name: "language",
-            type: "enum"
-        }
+        { name: "language", type: "enum" }
     ],
     execute: (_player, [language]) => {
-        if (!Object.keys(languageList).includes(language)) return { status: 1, message: "§7[§aMatrix§7] §fUnsupported or invalid language!" };
+        if (!Object.keys(languageList).includes(language)) {
+            return {
+                status: 1,
+                message: "§7[§aMatrix§7] §f" + text("commandLanguageInvalid", language)
+            };
+        }
+
         world.setDynamicProperty("database:systemLanguage", language);
         updateLanguage();
-        return { status: 0, message: "§7[§aMatrix§7] §fSwitched language to §e" + language };
+
+        return {
+            status: 0,
+            message: "§7[§aMatrix§7] §f" + text("commandLanguageSuccess", language)
+        };
     }
 } as Command;
