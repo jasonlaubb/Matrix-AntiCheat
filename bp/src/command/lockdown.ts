194eba4008e714bb20747b0d2fd9c01e930e715b
jasonlaubb <jasonlau5378@gmail.com>
2025-09-13 08:44:59 +0800

    awa

--- bp/src/command/lockdown.ts
+++ bp/src/command/lockdown.ts
@@ -1,15 +1,28 @@
+import english from "../data/languages/english";
 import type { Command } from "../main";
 import { world } from "@minecraft/server";
+import { text } from "../util/text";
 export default {
     name: "lockdown",
-    description: "Lockdown the server",
+    description: english.commandLockdownDescription,
     requireOp: true,
+    translationDef: {
+        actionName: "commandLockdown",
+        description: "commandLockdownDescription"
+    },
     execute: () => {
         if (world?.lockdown) {
             delete world.lockdown;
-            return { status: 0, message: "§7[§aMatrix§7] §fServer is no longer locked down." };
+            return {
+                status: 0,
+                message: "§7[§aMatrix§7] §f" + text("commandLockdownDisabled")
+            };
         }
+
         world.lockdown = true;
-        return { status: 0, message: "§7[§aMatrix§7] §fServer is now locked down, new players except operator will be kicked." };
+        return {
+            status: 0,
+            message: "§7[§aMatrix§7] §f" + text("commandLockdownEnabled")
+        };
     },
 } as Command;
--- bp/src/command/mute.ts
+++ bp/src/command/mute.ts
@@ -3,57 +3,100 @@ import { timeUnits } from "./ban";
 import type { Command } from "../main";
 import { parseTime, hasEducationalFeature } from "../util/util";
 import { checkPunish } from "../util/punishment";
-
+import english from "../data/languages/english";
+import { text } from "../util/text";
 export const mute = {
     name: "mute",
-    description: "Mute a player",
-    parameters: [
-        {
-            name: "player",
-            type: "playerTarget",
-        },
-    ],
+    description: english.commandMuteDescription,
     requireOp: true,
+    translationDef: {
+        actionName: "commandMute",
+        description: "commandMuteDescription",
+        param: ["commandMuteTarget"],
+        optionalParam: ["commandMuteDuration", "commandMuteTimeUnit"]
+    },
+    parameters: [
+        { name: "player", type: "playerTarget" },
+    ],
     optionalParameters: [
-        {
-            name: "duration",
-            type: "float",
-            min: 1,
-        },
-        {
-            name: "timeUnit",
-            type: "enum",
-        },
+        { name: "duration", type: "float", min: 1 },
+        { name: "timeUnit", type: "enum" },
     ],
     execute: (_player, [target, duration, timeUnit]) => {
-        if (!hasEducationalFeature()) return { status: 1, message: "§7[§aMatrix§7] §fEducation edition required! " };
-        if (target.getDynamicProperty("muteData:" + target.id)) return { status: 1, message: "§7[§aMatrix§7] §fPlayer has been muted already." };
-        if (duration && !timeUnit) return { status: 1, message: "§7[§aMatrix§7] §fYou need to type time unit if you want to set a duration." };
-        if (duration && !timeUnits.includes(timeUnit)) return { status: 1, message: "§7[§aMatrix§7] §fInvalid time unit!" };
+        if (!hasEducationalFeature()) {
+            return {
+                status: 1,
+                message: "§7[§aMatrix§7] §f" + text("commandMuteMissingEdu")
+            };
+        }
+
+        if (target.getDynamicProperty("muteData:" + target.id)) {
+            return {
+                status: 1,
+                message: "§7[§aMatrix§7] §f" + text("commandMuteAlready", target.name)
+            };
+        }
+
+        if (duration && !timeUnit) {
+            return {
+                status: 1,
+                message: "§7[§aMatrix§7] §f" + text("commandMuteMissingUnit")
+            };
+        }
+
+        if (duration && !timeUnits.includes(timeUnit)) {
+            return {
+                status: 1,
+                message: "§7[§aMatrix§7] §f" + text("commandMuteInvalidUnit", timeUnit)
+            };
+        }
+
         system.run(() => {
             target.setDynamicProperty("muteData:" + target.id, duration ? Date.now() + parseTime(timeUnit, duration) : -1);
             checkPunish(target);
         });
-        return { status: 0, message: "§7[§aMatrix§7] §fMuted player: " + target.name };
+
+        return {
+            status: 0,
+            message: "§7[§aMatrix§7] §f" + text("commandMuteSuccess", target.name)
+        };
     },
 } as Command;
 export const unmute = {
     name: "unmute",
-    description: "Unmute a player",
+    description: english.commandUnmuteDescription,
     requireOp: true,
+    translationDef: {
+        actionName: "commandUnmute",
+        description: "commandUnmuteDescription",
+        param: ["commandUnmuteTarget"]
+    },
     parameters: [
-        {
-            name: "player",
-            type: "playerTarget",
-        },
+        { name: "player", type: "playerTarget" },
     ],
     execute: (_player, [target]) => {
-        if (!hasEducationalFeature()) return { status: 1, message: "§7[§aMatrix§7] §fEducation edition required! " };
-        if (!target.getDynamicProperty("muteData:" + target.id)) return { status: 1, message: "§7[§aMatrix§7] §fPlayer has not been muted." };
+        if (!hasEducationalFeature()) {
+            return {
+                status: 1,
+                message: "§7[§aMatrix§7] §f" + text("commandMuteMissingEdu")
+            };
+        }
+
+        if (!target.getDynamicProperty("muteData:" + target.id)) {
+            return {
+                status: 1,
+                message: "§7[§aMatrix§7] §f" + text("commandUnmuteNotMuted", target.name)
+            };
+        }
+
         system.run(() => {
             target.setDynamicProperty("muteData:" + target.id);
             target.runCommand("ability @s mute false");
         });
-        return { status: 0, message: "§7[§aMatrix§7] §fMuted player: " + target.name };
+
+        return {
+            status: 0,
+            message: "§7[§aMatrix§7] §f" + text("commandUnmuteSuccess", target.name)
+        };
     },
 } as Command;
