12312a7e4d212e374cf22f8921ab7ea8883d6f50
jasonlaubb <jasonlau5378@gmail.com>
2025-09-16 17:03:38 +0800

    awa

--- bp/src/data/prototype.ts
+++ bp/src/data/prototype.ts
@@ -8,7 +8,7 @@ Player.prototype.isOp = function () {
     return this.commandPermissionLevel >= 1 || this.playerPermissionLevel === PlayerPermissionLevel.Operator;
 };
 Player.prototype.flag = function (id: string, type: string, category: string, data?: { [key: string]: string | number }) {
-    const flagMessage = `§7[§aMatrix§7] §f${text("flagDetected", this.name + "§r")} §7<${category}> §c[${id}/${type}]${data ? ` §9(${Object.entries(data).map(([k, v]) => `${k}=${v}§r§9`)})` : ""}`;
+    const flagMessage = `§7[§aMatrix§7] §f${text("flagDetected", this.name + "§r")} §7<${category}> §c[${id}/${type}]${data && !get("shortenFlagMessage") ? ` §9(${Object.entries(data).map(([k, v]) => `${k}=${v}§r§9`)})` : ""}`;
     const flagType = get("flagMessageTarget");
     let flagTarget: Player[] = [];
     switch (flagType) {
